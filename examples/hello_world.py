41d10e2b243555242b9b4d6ba22269ba6da6502f
Brenden Blanco <bblanco@plumgrid.com>
2015-06-05 13:28:17 -0700

    Add simple single file example
    Signed-off-by: Brenden Blanco <bblanco@plumgrid.com>

---
+++ examples/hello_world.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Copyright (c) PLUMgrid, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License")
+
+# run in project directory with:
+# sudo bash -c "PYTHONPATH=$PWD/src LD_LIBRARY_PATH=$PWD/build/src/cc examples/hello_world.py"
+
+from bpf import BPF
+from subprocess import call
+
+prog = """
+#include "src/cc/bpf_helpers.h"
+BPF_EXPORT(hello)
+int hello(void *ctx) {
+  char fmt[] = "Hello, World!\\n";
+  bpf_trace_printk(fmt, sizeof(fmt));
+  return 0;
+};
+"""
+b = BPF(text=prog)
+fn = b.load_func("hello", BPF.KPROBE)
+BPF.attach_kprobe(fn, "sys_clone")
+call(["cat", "/sys/kernel/debug/tracing/trace_pipe"])
