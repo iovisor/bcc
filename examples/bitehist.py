f32a67c96ddf09252c2b2e850b0ffdfedfb39543
Brendan Gregg <brendan.d.gregg@gmail.com>
2015-09-07 14:42:12 -0700

    make bitehist a simpler example of log2 histograms

--- examples/bitehist.py
+++ examples/bitehist.py
@@ -5,8 +5,6 @@
 #
 # Written as a basic example of using a histogram to show a distribution.
 #
-# USAGE: bitehist.py [interval [count]]
-#
 # The default interval is 5 seconds. A Ctrl-C will print the partially
 # gathered histogram then exit.
 #
@@ -17,24 +15,6 @@
 
 from bcc import BPF
 from time import sleep
-from sys import argv
-
-def usage():
-	print("USAGE: %s [interval [count]]" % argv[0])
-	exit()
-
-# arguments
-interval = 5
-count = -1
-if len(argv) > 1:
-	try:
-		interval = int(argv[1])
-		if interval == 0:
-			raise
-		if len(argv) > 2:
-			count = int(argv[2])
-	except:	# also catches -h, --help
-		usage()
 
 # load BPF program
 b = BPF(src_file = "bitehist.c")
@@ -44,20 +24,10 @@ b.attach_kprobe(event="blk_start_request", fn_name="do_request")
 print("Tracing... Hit Ctrl-C to end.")
 
 # output
-loop = 0
-do_exit = 0
-while (1):
-	if count > 0:
-		loop += 1
-		if loop > count:
-			exit()
-	try:
-		sleep(interval)
-	except KeyboardInterrupt:
-		pass; do_exit = 1
-
+try:
+	sleep(99999999)
+except KeyboardInterrupt:
 	print
-	b["dist"].print_log2_hist()
-	b["dist"].clear()
-	if do_exit:
-		exit()
+
+b["dist"].print_log2_hist()
+b["dist"].clear()
