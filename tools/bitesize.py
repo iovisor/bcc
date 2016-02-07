6497a84e79113e05cf3be6da98fe973f0ea86c67
Brendan Gregg <brendan.d.gregg@gmail.com>
2016-02-06 17:08:38 -0800

    bitesize nits

--- tools/bitesize.py
+++ tools/bitesize.py
@@ -4,8 +4,8 @@
 #               For Linux, uses BCC, eBPF. See .c file.
 #
 # USAGE: bitesize
-# Ctrl-C will print the partially gathered histogram then exit.
 #
+# Ctrl-C will print the partially gathered histogram then exit.
 #
 # Copyright (c) 2016 Allan McAleavy
 # Licensed under the Apache License, Version 2.0 (the "License")
@@ -46,7 +46,7 @@ int do_count (struct pt_regs *ctx, struct request *req)
     struct val_t *valp;
 
     valp = commbyreq.lookup(&req);
-    if ( valp ==  0) {
+    if (valp ==  0) {
        return 0;
     }
 
@@ -55,7 +55,7 @@ int do_count (struct pt_regs *ctx, struct request *req)
         bpf_probe_read(&key.name, sizeof(key.name),valp->name);
         dist.increment(key);
     }
-       return 0;
+    return 0;
 }
 """
 
@@ -72,4 +72,4 @@ dist = b.get_table("dist")
 try:
     sleep(99999999)
 except KeyboardInterrupt:
-    dist.print_log2_hist("Kbytes", "Process Name:")
+    dist.print_log2_hist("Kbytes", "Process Name")
