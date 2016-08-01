5a599a4726f57e7f57b4e000482a0d6039ff5a91
Brendan Gregg <brendan.d.gregg@gmail.com>
2016-08-01 11:47:28 -0700

    add reference to mysqld example

--- examples/tracing/mysqld_query.py
+++ examples/tracing/mysqld_query.py
@@ -26,6 +26,12 @@ bpf_text = """
 int do_trace(struct pt_regs *ctx) {
     uint64_t addr;
     char query[128];
+    /*
+     * Read the first argument from the query-start probe, which is the query.
+     * The format of this probe is:
+     * query-start(query, connectionid, database, user, host)
+     * see: https://dev.mysql.com/doc/refman/5.7/en/dba-dtrace-ref-query.html
+     */
     bpf_usdt_readarg(1, ctx, &addr);
     bpf_trace_printk("%s\\n", addr);
     return 0;
