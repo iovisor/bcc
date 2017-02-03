8caa755d821e038cdd644c134a087153dbd49b8b
jsedgwick <jsedgwick@fb.com>
2017-02-03 11:57:53 -0800

    remove symbol name storage optimization
    Summary: not only is this implementation buggy (modifying the map invalidates pointers into it) but the cost of maintaining the map outstrips any performance benefits, according to perf. remove symnames_ and simplify implementation.
    
    Test Plan: unit tests, used in my project

--- src/cc/syms.h
+++ src/cc/syms.h
@@ -66,9 +66,9 @@ public:
 
 class ProcSyms : SymbolCache {
   struct Symbol {
-    Symbol(const std::string *name, uint64_t start, uint64_t size, int flags = 0)
+    Symbol(const char *name, uint64_t start, uint64_t size, int flags = 0)
         : name(name), start(start), size(size), flags(flags) {}
-    const std::string *name;
+    std::string name;
     uint64_t start;
     uint64_t size;
     int flags;
@@ -84,7 +84,6 @@ class ProcSyms : SymbolCache {
     std::string name_;
     uint64_t start_;
     uint64_t end_;
-    std::unordered_set<std::string> symnames_;
     std::vector<Symbol> syms_;
 
     void load_sym_table();
