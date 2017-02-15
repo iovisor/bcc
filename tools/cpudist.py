af236e7d047f5f8bdba578136856eb566dcde048
Rafael Fonseca <rdossant@redhat.com>
2017-02-15 17:28:26 +0100

    Revert "undo rebase"
    This reverts commit a112514cba8e4844c405e3f1bd341289c4d2a158.
    
    The changes the commit reverted are legit and should not have been
    reverted.

--- tools/cpudist.py
+++ tools/cpudist.py
@@ -12,7 +12,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License")
 
 from __future__ import print_function
-from bcc import BPF, Tracepoint
+from bcc import BPF
 from time import sleep, strftime
 import argparse
 
