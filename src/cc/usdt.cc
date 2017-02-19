8698bdb021cd3385e6eeac64e438b66d840721c0
Sasha Goldshtein <goldshtn@gmail.com>
2017-02-19 20:29:45 +0000

    Support base + index * scale addressing for USDT arguments
    It turns out that some software will have USDT probe arguments
    referencing memory using the full `nnn@(%basereg + %idxreg * scale`
    syntax. This is represented as `nnn@(%basereg,%idxreg,scale)` in
    the `NT_STAPSDT` note, encountered in building a recent version of
    PostgreSQL on FC25.
    
    This format is now recognized by the USDT parser, and the correct
    BPF code is emitted to retrieve arguments that reference memory
    using this full addressing syntax.`

--- src/cc/usdt.cc
+++ src/cc/usdt.cc
@@ -360,48 +360,56 @@ void bcc_usdt_foreach(void *usdt, bcc_usdt_cb callback) {
 
 int bcc_usdt_get_location(void *usdt, const char *probe_name,
                           int index, struct bcc_usdt_location *location) {
-    USDT::Context *ctx = static_cast<USDT::Context *>(usdt);
-    USDT::Probe *probe = ctx->get(probe_name);
-    if (!probe)
-        return -1;
-    if (index < 0 || (size_t)index >= probe->num_locations())
-        return -1;
-    location->address = probe->address(index);
-    return 0;
+  USDT::Context *ctx = static_cast<USDT::Context *>(usdt);
+  USDT::Probe *probe = ctx->get(probe_name);
+  if (!probe)
+    return -1;
+  if (index < 0 || (size_t)index >= probe->num_locations())
+    return -1;
+  location->address = probe->address(index);
+  return 0;
 }
 
 int bcc_usdt_get_argument(void *usdt, const char *probe_name,
                           int location_index, int argument_index,
                           struct bcc_usdt_argument *argument) {
-    USDT::Context *ctx = static_cast<USDT::Context *>(usdt);
-    USDT::Probe *probe = ctx->get(probe_name);
-    if (!probe)
-        return -1;
-    if (argument_index < 0 || (size_t)argument_index >= probe->num_arguments())
-        return -1;
-    if (location_index < 0 || (size_t)location_index >= probe->num_locations())
-        return -1;
-    auto const &location = probe->location(location_index);
-    auto const &arg = location.arguments_[argument_index];
-    argument->size = arg.arg_size();
-    argument->valid = BCC_USDT_ARGUMENT_NONE;
-    if (arg.constant()) {
-        argument->valid |= BCC_USDT_ARGUMENT_CONSTANT;
-        argument->constant = *(arg.constant());
-    }
-    if (arg.deref_offset()) {
-        argument->valid |= BCC_USDT_ARGUMENT_DEREF_OFFSET;
-        argument->deref_offset = *(arg.deref_offset());
-    }
-    if (arg.deref_ident()) {
-        argument->valid |= BCC_USDT_ARGUMENT_DEREF_IDENT;
-        argument->deref_ident = arg.deref_ident()->c_str();
-    }
-    if (arg.register_name()) {
-        argument->valid |= BCC_USDT_ARGUMENT_REGISTER_NAME;
-        argument->register_name = arg.register_name()->c_str();
-    }
-    return 0;
+  USDT::Context *ctx = static_cast<USDT::Context *>(usdt);
+  USDT::Probe *probe = ctx->get(probe_name);
+  if (!probe)
+    return -1;
+  if (argument_index < 0 || (size_t)argument_index >= probe->num_arguments())
+    return -1;
+  if (location_index < 0 || (size_t)location_index >= probe->num_locations())
+    return -1;
+  auto const &location = probe->location(location_index);
+  auto const &arg = location.arguments_[argument_index];
+  argument->size = arg.arg_size();
+  argument->valid = BCC_USDT_ARGUMENT_NONE;
+  if (arg.constant()) {
+    argument->valid |= BCC_USDT_ARGUMENT_CONSTANT;
+    argument->constant = *(arg.constant());
+  }
+  if (arg.deref_offset()) {
+    argument->valid |= BCC_USDT_ARGUMENT_DEREF_OFFSET;
+    argument->deref_offset = *(arg.deref_offset());
+  }
+  if (arg.deref_ident()) {
+    argument->valid |= BCC_USDT_ARGUMENT_DEREF_IDENT;
+    argument->deref_ident = arg.deref_ident()->c_str();
+  }
+  if (arg.base_register_name()) {
+    argument->valid |= BCC_USDT_ARGUMENT_BASE_REGISTER_NAME;
+    argument->base_register_name = arg.base_register_name()->c_str();
+  }
+  if (arg.index_register_name()) {
+    argument->valid |= BCC_USDT_ARGUMENT_INDEX_REGISTER_NAME;
+    argument->index_register_name = arg.index_register_name()->c_str();
+  }
+  if (arg.scale()) {
+    argument->valid |= BCC_USDT_ARGUMENT_SCALE;
+    argument->scale = *(arg.scale());
+  }
+  return 0;
 }
 
 void bcc_usdt_foreach_uprobe(void *usdt, bcc_usdt_uprobe_cb callback) {
