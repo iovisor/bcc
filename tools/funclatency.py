iovisor/bcc

commit d73c58f0c8c754bd591264205ac2ddc83c1d2c7f

Use PID to resolve path of target libraries
Leverage commit 690b7e6d

tools/funclatency.py
@@ -201,9 +201,10 @@ if not library:
     b.attach_kretprobe(event_re=pattern, fn_name="trace_func_return")
     matched = b.num_open_kprobes()
 else:
-    b.attach_uprobe(name=library, sym_re=pattern, fn_name="trace_func_entry")
+    b.attach_uprobe(name=library, sym_re=pattern, fn_name="trace_func_entry",
+                    pid=args.pid or -1)
     b.attach_uretprobe(name=library, sym_re=pattern,
-                       fn_name="trace_func_return")
+                       fn_name="trace_func_return", pid=args.pid or -1)
     matched = b.num_open_uprobes()
 
 if matched == 0:

tools/sslsniff.py
@@ -130,18 +130,20 @@ b = BPF(text=prog)
 # on its exit (Mark Drayton)
 #
 if args.openssl:
-    b.attach_uprobe(name="ssl", sym="SSL_write", fn_name="probe_SSL_write")
-    b.attach_uprobe(name="ssl", sym="SSL_read", fn_name="probe_SSL_read_enter")
+    b.attach_uprobe(name="ssl", sym="SSL_write", fn_name="probe_SSL_write",
+                    pid=args.pid or -1)
+    b.attach_uprobe(name="ssl", sym="SSL_read", fn_name="probe_SSL_read_enter",
+                    pid=args.pid or -1)
     b.attach_uretprobe(name="ssl", sym="SSL_read",
-                       fn_name="probe_SSL_read_exit")
+                       fn_name="probe_SSL_read_exit", pid=args.pid or -1)
 
 if args.gnutls:
     b.attach_uprobe(name="gnutls", sym="gnutls_record_send",
-                    fn_name="probe_SSL_write")
+                    fn_name="probe_SSL_write", pid=args.pid or -1)
     b.attach_uprobe(name="gnutls", sym="gnutls_record_recv",
-                    fn_name="probe_SSL_read_enter")
+                    fn_name="probe_SSL_read_enter", pid=args.pid or -1)
     b.attach_uretprobe(name="gnutls", sym="gnutls_record_recv",
-                       fn_name="probe_SSL_read_exit")
+                       fn_name="probe_SSL_read_exit", pid=args.pid or -1)
 
 # define output data structure in Python
 TASK_COMM_LEN = 16  # linux/sched.h

commit 5ac5d6e9e1d65415d3b813930accab39acb9ad86

funclatency/sslsniff: fix the TypeError when enabling pid filter on uprobe
attach_uprobe() and attach_uretprobe() expect the type of pid argument
is "int" instead of "str". Fix it by specifying the type of the "-p"/"--pid"
option as "int", and updating the format string of args.pid accordingly.

Fixes: d73c58f0c8c754bd591264205ac2ddc83c1d2c7f

tools/funclatency.py
@@ -45,7 +45,7 @@ parser = argparse.ArgumentParser(
     description="Time functions and print latency as a histogram",
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog=examples)
-parser.add_argument("-p", "--pid",
+parser.add_argument("-p", "--pid", type=int,
     help="trace this PID only")
 parser.add_argument("-i", "--interval", default=99999999,
     help="summary interval, seconds")
@@ -147,7 +147,7 @@ need_key = args.function or (library and not args.pid)
 # code substitutions
 if args.pid:
     bpf_text = bpf_text.replace('FILTER',
-        'if (tgid != %s) { return 0; }' % args.pid)
+        'if (tgid != %d) { return 0; }' % args.pid)
 else:
     bpf_text = bpf_text.replace('FILTER', '')
 if args.milliseconds:

tools/sslsniff.py
@@ -30,7 +30,7 @@ parser = argparse.ArgumentParser(
     description="Sniff SSL data",
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog=examples)
-parser.add_argument("-p", "--pid", help="sniff this PID only.")
+parser.add_argument("-p", "--pid", type=int, help="sniff this PID only.")
 parser.add_argument("-c", "--comm",
                     help="sniff only commands matching string.")
 parser.add_argument("-o", "--no-openssl", action="store_false", dest="openssl",
@@ -115,7 +115,7 @@ int probe_SSL_read_exit(struct pt_regs *ctx, void *ssl, void *buf, int num) {
 """
 
 if args.pid:
-    prog = prog.replace('FILTER', 'if (pid != %s) { return 0; }' % args.pid)
+    prog = prog.replace('FILTER', 'if (pid != %d) { return 0; }' % args.pid)
 else:
     prog = prog.replace('FILTER', '')
 