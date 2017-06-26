25f3ccee72f50f14fd51166a9839f86570be9f72
Brenden Blanco <bblanco@gmail.com>
2017-06-26 13:37:34 -0700

    src/cc: cmake file cleanup and split usdt into subdir
    Move two usdt related files to a new subdirectory and link them into the
    final product. Introduce a cmake option to disable that feature (turning
    it off currently fails to compile).
    
    Move some of the cmake flag computation into a helper cmake file and
    include that rather than inlining it.
    
    Signed-off-by: Brenden Blanco <bblanco@gmail.com>

---
+++ cmake/static_libstdc++.cmake
@@ -0,0 +1,15 @@
+# only turn on static-libstdc++ if also linking statically against clang
+string(REGEX MATCH ".*[.]a$" LIBCLANG_ISSTATIC "${libclangBasic}")
+# if gcc 4.9 or higher is used, static libstdc++ is a good option
+if (CMAKE_COMPILER_IS_GNUCC AND LIBCLANG_ISSTATIC)
+  execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
+  if (GCC_VERSION VERSION_GREATER 4.9 OR GCC_VERSION VERSION_EQUAL 4.9)
+    execute_process(COMMAND ${CMAKE_C_COMPILER} -print-libgcc-file-name OUTPUT_VARIABLE GCC_LIB)
+    get_filename_component(GCC_DIR "${GCC_LIB}" DIRECTORY)
+    find_library(GCC_LIBSTDCPP libstdc++.a PATHS "${GCC_DIR}" NO_DEFAULT_PATH)
+    if (GCC_LIBSTDCPP)
+      message(STATUS "Using static-libstdc++")
+      set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -static-libstdc++")
+    endif()
+  endif()
+endif()
