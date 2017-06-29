71fc3d5c890f57fb7f979c6176d97da0f04d0801
Brenden Blanco <bblanco@gmail.com>
2017-06-28 17:37:06 -0700

    move api and create dependent option
    Move the C++ api files under a new subdirectory.
    Use CMAKE_DEPENDENT_OPTION to enforce sdt->cpp_api relationship.
    
    Since linking .a into a .so can cause global symbols to be dropped, add
    a helper file to force exported symbols to be retained (link_all.cc).
    This problem doesn't exist for building the static cpp examples.
    
    Signed-off-by: Brenden Blanco <bblanco@gmail.com>

---
+++ src/cc/link_all.cc
@@ -0,0 +1,21 @@
+// Copyright (c) 2017 VMware, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License")
+#include <cstdlib>
+
+#include "bcc_usdt.h"
+
+namespace {
+  // Take this trick from llvm for forcing exported functions in helper
+  // libraries to be included in the final .so
+  struct LinkAll {
+    LinkAll() {
+      // getenv never returns -1, but compiler doesn't know!
+      if (::getenv("bar") != (char *)-1)
+        return;
+
+      (void)bcc_usdt_new_frompid(-1);
+      (void)bcc_usdt_new_frompath(nullptr);
+      (void)bcc_usdt_close(nullptr);
+    }
+  } LinkAll;  // declare one instance to invoke the constructor
+}
