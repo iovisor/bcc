6fb540a02b6d188b03db2e747142d47cff86c5ec
Igor Mazur <igor.mazur@grammarly.com>
2017-07-12 15:45:07 +0300

    MySQL uprobe: Code deduplication, comments

--- tools/dbslower.py
+++ tools/dbslower.py
@@ -34,7 +34,7 @@ examples = """examples:
     dbslower postgres -p 188 322 # trace specific PostgreSQL processes
     dbslower mysql -p 480 -m 30  # trace MySQL queries slower than 30ms
     dbslower mysql -p 480 -v     # trace MySQL queries & print the BPF program
-    dbslower mysql -b $(which mysqld)  # trace MySQL queries with uprobes
+    dbslower mysql -x $(which mysqld)  # trace MySQL queries with uprobes
 """
 parser = argparse.ArgumentParser(
     description="",
@@ -46,7 +46,7 @@ parser.add_argument("db", choices=["mysql", "postgres"],
     help="the database engine to use")
 parser.add_argument("-p", "--pid", type=int, nargs='*',
     dest="pids", metavar="PID", help="the pid(s) to trace")
-parser.add_argument("-b", "--binary", type=str,
+parser.add_argument("-x", "--exe", type=str,
     dest="path", metavar="PATH", help="path to binary")
 parser.add_argument("-m", "--threshold", type=int, default=1,
     help="trace queries slower than this threshold (ms)")
@@ -54,161 +54,125 @@ args = parser.parse_args()
 
 threshold_ns = args.threshold * 1000000
 
-program_uprobe = """
-#include <uapi/linux/ptrace.h>
-
-struct temp_t {
-    u64 timestamp;
-    char query[256];
-};
-
-struct data_t {
-    u64 pid;
-    u64 timestamp;
-    u64 duration;
-    char query[256];
-};
-
-BPF_HASH(temp, u64, struct temp_t);
-BPF_PERF_OUTPUT(events);
-
-int mysql56_dispatch_start(struct pt_regs *ctx) {
-    u64 pid = bpf_get_current_pid_tgid();
-    u64 command  = (u64) PT_REGS_PARM1(ctx);
-
-    if (command == 3) {
-        struct temp_t tmp = {};
-        tmp.timestamp = bpf_ktime_get_ns();
-        bpf_probe_read(&tmp.query, sizeof(tmp.query), (void*) PT_REGS_PARM3(ctx));
-
-        temp.update(&pid, &tmp);
-    }
-    return 0;
-};
-
-int mysql57_dispatch_start(struct pt_regs *ctx) {
-    u64 pid = bpf_get_current_pid_tgid();
-    u64 command  = (u64) PT_REGS_PARM3(ctx);
-
-    if (command == 3) {
-        struct temp_t tmp = {};
-        tmp.timestamp = bpf_ktime_get_ns();
-
-        void* st = (void*) PT_REGS_PARM2(ctx);
-        char* query;
-        bpf_probe_read(&query, sizeof(query), st);
-        bpf_probe_read(&tmp.query, sizeof(tmp.query), query);
-
-        temp.update(&pid, &tmp);
-    }
-    return 0;
-};
-
-int dispatch_end(struct pt_regs *ctx) {
-    struct temp_t *tempp;
-    u64 pid = bpf_get_current_pid_tgid();
-    tempp = temp.lookup(&pid);
-    if (!tempp)
-        return 0;
-
-    u64 delta = bpf_ktime_get_ns() - tempp->timestamp;
-    if (delta >= """ + str(threshold_ns) + """) {
-        struct data_t data = {};
-        data.pid = pid >> 32;   // only process id
-        data.timestamp = tempp->timestamp;
-        data.duration = delta;
-        bpf_probe_read(&data.query, sizeof(data.query), tempp->query);
-        events.perf_submit(ctx, &data, sizeof(data));
-    }
-    temp.delete(&pid);
-    return 0;
-};
-
-"""
-
-program_udst = """
-#include <uapi/linux/ptrace.h>
-
-struct temp_t {
-    u64 timestamp;
-    char *query;
-};
-
-struct data_t {
-    u64 pid;
-    u64 timestamp;
-    u64 duration;
-    char query[256];
-};
-
-BPF_HASH(temp, u64, struct temp_t);
-BPF_PERF_OUTPUT(events);
-
-int probe_start(struct pt_regs *ctx) {
-    struct temp_t tmp = {};
-    tmp.timestamp = bpf_ktime_get_ns();
-    bpf_usdt_readarg(1, ctx, &tmp.query);
-    u64 pid = bpf_get_current_pid_tgid();
-    temp.update(&pid, &tmp);
-    return 0;
-}
-
-int probe_end(struct pt_regs *ctx) {
-    struct temp_t *tempp;
-    u64 pid = bpf_get_current_pid_tgid();
-    tempp = temp.lookup(&pid);
-    if (!tempp)
-        return 0;
-
-    u64 delta = bpf_ktime_get_ns() - tempp->timestamp;
-    if (delta >= """ + str(threshold_ns) + """) {
-        struct data_t data = {};
-        data.pid = pid >> 32;   // only process id
-        data.timestamp = tempp->timestamp;
-        data.duration = delta;
-        bpf_probe_read(&data.query, sizeof(data.query), tempp->query);
-        events.perf_submit(ctx, &data, sizeof(data));
-    }
-    temp.delete(&pid);
-    return 0;
-}
-"""
-
+mode = "USDT"
 if args.path and not args.pids:
-    # Uprobes mode
-    program = program_uprobe
-
-    symbols = subprocess.check_output(["nm", "-aD", args.path])
-    bpf = BPF(text=program)
     if args.db == "mysql":
-        dispatch_fname = [name for name in symbols.split('\n') if name.find("dispatch_command") >= 0]
+        symbols = BPF.get_user_functions_and_addresses(args.path, "\\w+dispatch_command\\w+")
 
-        if len(dispatch_fname) == 0:
+        if len(symbols) == 0:
             print("Cant find function 'dispatch_command' in %s" % (args.path))
             exit(1)
+        
+        (mysql_func_name, addr) = symbols[0]
 
-        m = re.search("\\w+dispatch_command\\w+", dispatch_fname[0])
-        if m:
-            func_name = m.group(0)
+        if mysql_func_name.find("COM_DATA") >= 0:
+            mode = "MYSQL57"
         else:
-            print("Cant extract real 'dispatch_command' function name from %s" % (dispatch_fname[0]))
-            exit(1)
-
-        if func_name.find("COM_DATA") >= 0:
-            bpf.attach_uprobe(name=args.path, sym=func_name, fn_name="mysql57_dispatch_start")
-            bpf.attach_uretprobe(name=args.path, sym=func_name, fn_name="dispatch_end")
-        else:
-            bpf.attach_uprobe(name=args.path, sym=func_name, fn_name="mysql56_dispatch_start")
-            bpf.attach_uretprobe(name=args.path, sym=func_name, fn_name="dispatch_end")
+            mode = "MYSQL56"
     else:
         # Placeholder for PostrgeSQL
         # Look on functions initStringInfo, pgstat_report_activity, EndCommand, NullCommand
         print("Sorry at the moment PostgreSQL supports only USDT")
         exit(1)
+
+program = """
+#include <uapi/linux/ptrace.h>
+
+DEFINE_THRESHOLD
+DEFINE_USDT
+DEFINE_MYSQL56
+DEFINE_MYSQL57
+
+struct temp_t {
+    u64 timestamp;
+#ifdef USDT
+    char *query;
+#else
+    /*
+    MySQL clears query packet before uretprobe call - so copy query in advance
+    */
+    char query[256];
+#endif //USDT
+};
+
+struct data_t {
+    u64 pid;
+    u64 timestamp;
+    u64 duration;
+    char query[256];
+};
+
+BPF_HASH(temp, u64, struct temp_t);
+BPF_PERF_OUTPUT(events);
+
+int query_start(struct pt_regs *ctx) {
+
+#if defined(MYSQL56) || defined(MYSQL57)
+/*
+Trace only packets with enum_server_command == COM_QUERY 
+*/
+    #ifdef MYSQL56
+    u64 command  = (u64) PT_REGS_PARM1(ctx);
+    #else //MYSQL57
+    u64 command  = (u64) PT_REGS_PARM3(ctx);
+    #endif
+    if (command != 3) return 0;
+#endif
+
+    struct temp_t tmp = {};
+    tmp.timestamp = bpf_ktime_get_ns();
+
+#if defined(MYSQL56)
+    bpf_probe_read(&tmp.query, sizeof(tmp.query), (void*) PT_REGS_PARM3(ctx));
+#elif defined(MYSQL57)
+    void* st = (void*) PT_REGS_PARM2(ctx);
+    char* query;
+    bpf_probe_read(&query, sizeof(query), st);
+    bpf_probe_read(&tmp.query, sizeof(tmp.query), query);
+#else //USDT
+    bpf_usdt_readarg(1, ctx, &tmp.query);
+#endif
+
+    u64 pid = bpf_get_current_pid_tgid();
+    temp.update(&pid, &tmp);
+    return 0;
+}
+
+int query_end(struct pt_regs *ctx) {
+    struct temp_t *tempp;
+    u64 pid = bpf_get_current_pid_tgid();
+    tempp = temp.lookup(&pid);
+    if (!tempp)
+        return 0;
+
+    u64 delta = bpf_ktime_get_ns() - tempp->timestamp;
+#ifdef THRESHOLD
+    if (delta >= THRESHOLD) {
+#endif //THRESHOLD     
+        struct data_t data = {};
+        data.pid = pid >> 32;   // only process id
+        data.timestamp = tempp->timestamp;
+        data.duration = delta;
+        bpf_probe_read(&data.query, sizeof(data.query), tempp->query);
+        events.perf_submit(ctx, &data, sizeof(data));
+#ifdef THRESHOLD
+    }
+#endif //THRESHOLD
+    temp.delete(&pid);
+    return 0;
+};
+""".replace("DEFINE_USDT", "#define USDT" if mode == "USDT" else "") \
+   .replace("DEFINE_MYSQL56", "#define MYSQL56" if mode == "MYSQL56" else "") \
+   .replace("DEFINE_MYSQL57", "#define MYSQL57" if mode == "MYSQL57" else "") \
+   .replace("DEFINE_THRESHOLD", ("#define THRESHOLD " + str(threshold_ns)) if threshold_ns > 0 else "")
+
+if mode.startswith("MYSQL"):
+    # Uprobes mode
+    bpf = BPF(text=program)
+    bpf.attach_uprobe(name=args.path, sym=mysql_func_name, fn_name="query_start")
+    bpf.attach_uretprobe(name=args.path, sym=mysql_func_name, fn_name="query_end")
 else:
     # USDT mode
-    program = program_udst
-
     if not args.pids or len(args.pids) == 0:
         if args.db == "mysql":
             args.pids = map(int, subprocess.check_output(
@@ -219,8 +183,8 @@ else:
 
     usdts = map(lambda pid: USDT(pid=pid), args.pids)
     for usdt in usdts:
-        usdt.enable_probe("query__start", "probe_start")
-        usdt.enable_probe("query__done", "probe_end")
+        usdt.enable_probe("query__start", "query_start")
+        usdt.enable_probe("query__done", "query_end")
     if args.verbose:
         print('\n'.join(map(lambda u: u.get_text(), usdts)))
 
@@ -245,8 +209,13 @@ def print_event(cpu, data, size):
         float(event.timestamp - start) / 1000000000,
         event.pid, float(event.delta) / 1000000, event.query))
 
-# print("Tracing database queries for pids %s slower than %d ms..." %
-#       (', '.join(map(str, args.pids)), args.threshold))
+if mode.startswith("MYSQL"):
+    print("Tracing database queries for application %s slower than %d ms..." %
+        (args.path, args.threshold))
+else:
+    print("Tracing database queries for pids %s slower than %d ms..." %
+        (', '.join(map(str, args.pids)), args.threshold))
+
 print("%-14s %-6s %8s %s" % ("TIME(s)", "PID", "MS", "QUERY"))
 
 bpf["events"].open_perf_buffer(print_event, page_cnt=64)
