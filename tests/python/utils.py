20915627b01979d794a853cfdfb79b2fdfa69e7b
Sandipan Das <sandipan@linux.vnet.ibm.com>
2017-09-12 11:45:33 +0530

    Add a generic utility to check any binary availability
    In order to run, some test programs depend on the availability
    of binaries in locations that are part of PATH. So, we add a
    generic utility to simplify this.
    
    Signed-off-by: Sandipan Das <sandipan@linux.vnet.ibm.com>

--- tests/python/utils.py
+++ tests/python/utils.py
@@ -1,6 +1,12 @@
 from pyroute2 import NSPopen
 from distutils.spawn import find_executable
 
+def has_executable(name):
+    path = find_executable(name)
+    if path is None:
+        raise Exception(name + ": command not found")
+    return path
+
 class NSPopenWithCheck(NSPopen):
     """
     A wrapper for NSPopen that additionally checks if the program
@@ -11,8 +17,5 @@ class NSPopenWithCheck(NSPopen):
 
     def __init__(self, nsname, *argv, **kwarg):
         name = list(argv)[0][0]
-        path = find_executable(name)
-        if path is None:
-            raise Exception(name + ": command not found")
-        else:
-            super(NSPopenWithCheck, self).__init__(nsname, *argv, **kwarg)
+        has_executable(name)
+        super(NSPopenWithCheck, self).__init__(nsname, *argv, **kwarg)
