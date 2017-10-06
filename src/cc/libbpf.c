7bfac02c7fb3280049c1fedea6f9935ef03bdac9
Prashant Bhole <bhole_prashant_q7@lab.ntt.co.jp>
2017-10-06 14:45:21 +0900

    libbpf: return error when if_nametoindex returns 0
    sll_ifindex=0 matches any interface, which is not exepected here

--- src/cc/libbpf.c
+++ src/cc/libbpf.c
@@ -432,6 +432,11 @@ int bpf_open_raw_sock(const char *name)
   memset(&sll, 0, sizeof(sll));
   sll.sll_family = AF_PACKET;
   sll.sll_ifindex = if_nametoindex(name);
+  if (sll.sll_ifindex == 0) {
+    fprintf(stderr, "bpf: Resolving device name to index: %s\n", strerror(errno));
+    close(sock);
+    return -1;
+  }
   sll.sll_protocol = htons(ETH_P_ALL);
   if (bind(sock, (struct sockaddr *)&sll, sizeof(sll)) < 0) {
     printf("bind to %s: %s\n", name, strerror(errno));
