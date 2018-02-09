8268edbe00f618ae74fc55e63e828b362240ec7f
Liu Bo <liub.liubo@gmail.com>
2018-02-09 11:58:40 -0700

    urandomread.py: remove extra tail semicolon after TRACEPOINT_PROBE
    A function definition doesn't need a tail semicolon.
    
    Signed-off-by: Liu Bo <liub.liubo@gmail.com>

--- examples/tracing/urandomread.py
+++ examples/tracing/urandomread.py
@@ -20,7 +20,7 @@ TRACEPOINT_PROBE(random, urandom_read) {
     // args is from /sys/kernel/debug/tracing/events/random/urandom_read/format
     bpf_trace_printk("%d\\n", args->got_bits);
     return 0;
-};
+}
 """)
 
 # header
