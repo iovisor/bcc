08f51520fe069589f6a962fa50dbf5aac4a878ad
Smita Koralahalli Channabasappa <kcsmita94@gmail.com>
2018-08-22 09:47:19 -0400

    Update oomkill.py

--- tools/oomkill.py
+++ tools/oomkill.py
@@ -38,7 +38,6 @@ BPF_PERF_OUTPUT(events);
 
 void kprobe__oom_kill_process(struct pt_regs *ctx, struct oom_control *oc, const char *message)
 {
-    struct task_struct *chosen;
     unsigned long totalpages;
     struct task_struct *p = oc->chosen;
     struct data_t data = {};
