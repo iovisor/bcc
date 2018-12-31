cf029173a59f2ce84a3dfc2d866c5be3eafaf16f
Vijay Nag <vijunag@gmail.com>
2018-12-31 13:14:45 +0530

    Fix #2098 Added build_id support for BPF stackmap. A separate build_id stackmap can be created with the help of new macro BPF_STACK_TRACE_BUILDID The kernel BPF reports stacktrace in the structure bpf_stack_build_id. Changes have been made to BPF modules to support resolving symbols mentioned in the above format. An example tool is also available in tools/stack_buildid_example.py

--- src/cc/api/BPF.cc
+++ src/cc/api/BPF.cc
@@ -655,7 +655,6 @@ BPFStackTable BPF::get_stack_table(const std::string& name, bool use_debug_file,
 BPFStackBuildIdTable BPF::get_stackbuildid_table(const std::string &name, bool use_debug_file,
                                                  bool check_debug_file_crc) {
   TableStorage::iterator it;
-
   if (bpf_module_->table_storage().Find(Path({bpf_module_->id(), name}), it))
     return BPFStackBuildIdTable(it->second, use_debug_file, check_debug_file_crc, get_bsymcache());
   return BPFStackBuildIdTable({}, use_debug_file, check_debug_file_crc, get_bsymcache());
