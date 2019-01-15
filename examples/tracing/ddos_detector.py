d42194c66badbf2840b7aa83876cec86157f7af5
Jugurtha BELKALEM <39241012+jugurthab@users.noreply.github.com>
2019-01-15 09:40:03 +0100

    Fixing code alignment issues

--- examples/tracing/ddos_detector.py
+++ examples/tracing/ddos_detector.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 #
 # ddos_detector.py	DDOS dectection system.
-#		
 #
-# Written as a basic networking example of using ePBF to detect a potential DDOS attack against a system.
-#
+# Written as a basic networking example of using ePBF
+# to detect a potential DDOS attack against a system.
 #
 # Copyright (c) 2019 Jugurtha BELKALEM.
 # Licensed under the Apache License, Version 2.0 (the "License")
@@ -17,11 +16,13 @@ prog = """
 #include <linux/skbuff.h>
 #include <uapi/linux/ip.h>
 
-/* If We receive more than 100 succesive packets with a difference of timestamp between each one of them is less than 1000000ns - Trigger ALERT */
+/* If We receive more than 100 succesive packets with a difference of */ 
+/* timestamp between each one of them is less than 1000000ns */
+/* Trigger ALERT */
 #define MAX_NB_PACKETS 100
 #define LEGAL_DIFF_TIMESTAMP_PACKETS 1000000
 
-BPF_HASH(received_packets);
+BPF_HASH(rcv_packets);
 
 // define C structure
 struct detectionTimestamp {
@@ -31,45 +32,33 @@ struct detectionTimestamp {
 // use perf buffer (avoid using /sys/kernel/debug/tracing/trace_pipe)
 BPF_PERF_OUTPUT(events);
 
-int detect_ddos(struct pt_regs *ctx, struct sk_buff *skb, struct net_device *dev, struct packet_type *pt, struct net_device *orig_dev){
-
+int detect_ddos(struct pt_regs *ctx, struct sk_buff *skb){
     struct detectionTimestamp detectionTs = {};
-    u64 received_packets_nb = 0, received_packets_nb_inter=1, *received_packets_nb_ptr;	
-
-    u64 received_packets_timestamp_index = 1, received_packets_timestamp_inter=0, *received_packets_timestamp_ptr;
-
-
-    received_packets_nb_ptr = received_packets.lookup(&received_packets_nb);
-
-
-
-    received_packets_timestamp_ptr = received_packets.lookup(&received_packets_timestamp_index);
-
-
-    if(received_packets_nb_ptr != 0 && received_packets_timestamp_ptr != 0){
-        received_packets_nb_inter = *received_packets_nb_ptr;
-
-
-        received_packets_timestamp_inter = bpf_ktime_get_ns() - *received_packets_timestamp_ptr;
-
-        if(received_packets_timestamp_inter < LEGAL_DIFF_TIMESTAMP_PACKETS){
-            received_packets_nb_inter++;
+    // Counts number of received packets
+    u64 rcv_packets_nb = 0, rcv_packets_nb_inter=1, *rcv_packets_nb_ptr;
+    // Measures elapsed time between 2 successive received packets
+    u64 rcv_packets_ts_index = 1, rcv_packets_ts_inter=0, *rcv_packets_ts_ptr;
+    rcv_packets_nb_ptr = rcv_packets.lookup(&rcv_packets_nb);
+    rcv_packets_ts_ptr = rcv_packets.lookup(&rcv_packets_ts_index);
+    if(rcv_packets_nb_ptr != 0 && rcv_packets_ts_ptr != 0){
+        rcv_packets_nb_inter = *rcv_packets_nb_ptr;
+        rcv_packets_ts_inter = bpf_ktime_get_ns() - *rcv_packets_ts_ptr;
+        if(rcv_packets_ts_inter < LEGAL_DIFF_TIMESTAMP_PACKETS){
+            rcv_packets_nb_inter++;
         } else {
-            received_packets_nb_inter = 0;
+            rcv_packets_nb_inter = 0;
         }
-
-        if(received_packets_nb_inter > MAX_NB_PACKETS){
-            bpf_trace_printk("DDOS attack - number of packets : %d\\n", received_packets_nb_inter);
+        if(rcv_packets_nb_inter > MAX_NB_PACKETS){
+            // Get timestamp of DDOS detection
             detectionTs.ts = bpf_ktime_get_ns();
             events.perf_submit(ctx, &detectionTs, sizeof(detectionTs));
         }
-            received_packets.delete(&received_packets_nb);
-            received_packets.delete(&received_packets_timestamp_index);
+            rcv_packets.delete(&rcv_packets_nb);
+            rcv_packets.delete(&rcv_packets_ts_index);
         }
-    received_packets_timestamp_inter = bpf_ktime_get_ns();
-    received_packets.update(&received_packets_nb, &received_packets_nb_inter);
-    received_packets.update(&received_packets_timestamp_index, &received_packets_timestamp_inter);
-
+    rcv_packets_ts_inter = bpf_ktime_get_ns();
+    rcv_packets.update(&rcv_packets_nb, &rcv_packets_nb_inter);
+    rcv_packets.update(&rcv_packets_ts_index, &rcv_packets_ts_inter);
     return 0; // always return 0
 }
 """
@@ -80,10 +69,8 @@ b = BPF(text=prog)
 # Attach kprobe to kernel function and sets detect_ddos as kprobe handler
 b.attach_kprobe(event="ip_rcv", fn_name="detect_ddos")
 
-
 class DetectionTimestamp(ct.Structure):
     _fields_ = [("ts", ct.c_ulonglong)]
-
 
 # Show message when ePBF stats
 print("DDOS detector started ... Hit Ctrl-C to end!")
