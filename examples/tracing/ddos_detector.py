57b80dc8f7566f0682684e4794a70919380c0b6a
Jugurtha BELKALEM <39241012+jugurthab@users.noreply.github.com>
2019-01-16 12:05:04 +0100

    Adding code comments

--- examples/tracing/ddos_detector.py
+++ examples/tracing/ddos_detector.py
@@ -2,7 +2,7 @@
 #
 # ddos_detector.py	DDOS dectection system.
 #
-# Written as a basic networking example of using ePBF
+# Written as a basic tracing example of using ePBF
 # to detect a potential DDOS attack against a system.
 #
 # Copyright (c) 2019 Jugurtha BELKALEM.
@@ -12,32 +12,44 @@
 
 from bcc import BPF
 import ctypes as ct
+import datetime
 prog = """
 #include <linux/skbuff.h>
 #include <uapi/linux/ip.h>
-
-/* If We receive more than 100 succesive packets with a difference of */ 
-/* timestamp between each one of them is less than 1000000ns */
-/* Trigger ALERT */
-#define MAX_NB_PACKETS 100
+#define MAX_NB_PACKETS 1000
 #define LEGAL_DIFF_TIMESTAMP_PACKETS 1000000
 
 BPF_HASH(rcv_packets);
 
 // define C structure
-struct detectionTimestamp {
-    u64 ts;
+struct detectionPackets {
+    u64 nb_ddos_packets;
 };
 
-// use perf buffer (avoid using /sys/kernel/debug/tracing/trace_pipe)
 BPF_PERF_OUTPUT(events);
 
-int detect_ddos(struct pt_regs *ctx, struct sk_buff *skb){
-    struct detectionTimestamp detectionTs = {};
-    // Counts number of received packets
+int detect_ddos(struct pt_regs *ctx, void *skb){
+    struct detectionPackets detectionPacket = {};
+    // Used to count number of received packets
     u64 rcv_packets_nb = 0, rcv_packets_nb_inter=1, *rcv_packets_nb_ptr;
-    // Measures elapsed time between 2 successive received packets
+    // Used to measure elapsed time between 2 successive received packets
     u64 rcv_packets_ts_index = 1, rcv_packets_ts_inter=0, *rcv_packets_ts_ptr;
+    /* The algorithm analyses packets received by ip_rcv function */
+    /* and measures the difference in reception time between each packet. */
+    /* DDOS flooders send millions of packets such that difference of */
+    /* timestamp between 2 successive packets is so small */
+    /* (which is not like regular applications behaviour). */
+    /* This script looks for this difference in time and if it sees */
+    /* more than 1000 succesive packets with a difference */
+    /* of timestamp between each one of them less than 1000000ns, */
+    /* ------------------ It Triggers an ALERT -----------------*/
+    /* Those settings must be adapted depending on regular network traffic */
+    /* ------------------------------------------------------------------- */
+    /* Important: this is a rudimentary intrusion detection system, one can */
+    /* test a real case attack using hping3. However; if regular network */
+    /* traffic increases above predefined detection settings, a false */
+    /* positive alert will be triggered (an example would be the */
+    /* case of large file downloads)*/
     rcv_packets_nb_ptr = rcv_packets.lookup(&rcv_packets_nb);
     rcv_packets_ts_ptr = rcv_packets.lookup(&rcv_packets_ts_index);
     if(rcv_packets_nb_ptr != 0 && rcv_packets_ts_ptr != 0){
@@ -49,13 +61,10 @@ int detect_ddos(struct pt_regs *ctx, struct sk_buff *skb){
             rcv_packets_nb_inter = 0;
         }
         if(rcv_packets_nb_inter > MAX_NB_PACKETS){
-            // Get timestamp of DDOS detection
-            detectionTs.ts = bpf_ktime_get_ns();
-            events.perf_submit(ctx, &detectionTs, sizeof(detectionTs));
+            detectionPacket.nb_ddos_packets = rcv_packets_nb_inter;
+            events.perf_submit(ctx, &detectionPacket, sizeof(detectionPacket));
         }
-            rcv_packets.delete(&rcv_packets_nb);
-            rcv_packets.delete(&rcv_packets_ts_index);
-        }
+    }
     rcv_packets_ts_inter = bpf_ktime_get_ns();
     rcv_packets.update(&rcv_packets_nb, &rcv_packets_nb_inter);
     rcv_packets.update(&rcv_packets_ts_index, &rcv_packets_ts_inter);
@@ -70,16 +79,17 @@ b = BPF(text=prog)
 b.attach_kprobe(event="ip_rcv", fn_name="detect_ddos")
 
 class DetectionTimestamp(ct.Structure):
-    _fields_ = [("ts", ct.c_ulonglong)]
+    _fields_ = [("nb_ddos_packets", ct.c_ulonglong)]
 
 # Show message when ePBF stats
 print("DDOS detector started ... Hit Ctrl-C to end!")
 
-print("%-18s %-6s" % ("TIME(s)", "MESSAGE"))
+print("%-26s %-10s" % ("TIME(s)", "MESSAGE"))
 
 def trigger_alert_event(cpu, data, size):
     event = ct.cast(data, ct.POINTER(DetectionTimestamp)).contents
-    print("%-18s %-6s" % (event.ts, "Attack detected"))
+    print("%-26s %s %ld" % (datetime.datetime.now(), \
+    "DDOS Attack => nb of packets up to now : ", event.nb_ddos_packets))
 
 # loop with callback to trigger_alert_event
 b["events"].open_perf_buffer(trigger_alert_event)
