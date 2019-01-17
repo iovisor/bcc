89bf6f7b1eab3f820876f5dcf1d6a6feb211f982
Yonghong Song <yhs@fb.com>
2019-01-17 09:12:13 -0800

    enable -g by default for llvm 9
    This patch introduced two changes so that BTF is available to bcc
    starting from LLVM 9:
      . -g to enable BTF generation in LLVM
      . ProcessAllSections for RuntimeDyld so BTF is available to bcc
    
    The bcc will then be able to post process and load BTF into the
    kernel.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>

--- src/cc/bpf_module.cc
+++ src/cc/bpf_module.cc
@@ -239,8 +239,12 @@ int BPFModule::finalize() {
     return -1;
   }
 
+#if LLVM_MAJOR_VERSION >= 9
+  engine_->setProcessAllSections(true);
+#else
   if (flags_ & DEBUG_SOURCE)
     engine_->setProcessAllSections(true);
+#endif
 
   if (int rc = run_pass_manager(*mod))
     return rc;
