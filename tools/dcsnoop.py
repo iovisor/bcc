85f291880b151deb6bbbf0a6670d793e84100535
Xiaozhou Liu <liuxiaozhou@bytedance.com>
2019-02-14 19:29:12 +0800

    add `enum` support for generating perf event data struct
    Add enum type support so that scripts like tools/dcsnoop.py could be
    simplied.

--- tools/dcsnoop.py
+++ tools/dcsnoop.py
@@ -23,7 +23,6 @@
 from __future__ import print_function
 from bcc import BPF
 import argparse
-import ctypes as ct
 import re
 import time
 
@@ -123,17 +122,6 @@ int kretprobe__d_lookup(struct pt_regs *ctx)
 }
 """
 
-TASK_COMM_LEN = 16  # linux/sched.h
-MAX_FILE_LEN = 64  # see inline C
-
-class Data(ct.Structure):
-    _fields_ = [
-        ("pid", ct.c_uint),
-        ("type", ct.c_int),
-        ("comm", ct.c_char * TASK_COMM_LEN),
-        ("filename", ct.c_char * MAX_FILE_LEN),
-    ]
-
 if args.ebpf:
     print(bpf_text)
     exit()
@@ -151,7 +139,7 @@ mode_s = {
 start_ts = time.time()
 
 def print_event(cpu, data, size):
-    event = ct.cast(data, ct.POINTER(Data)).contents
+    event = b["events"].event(data)
     print("%-11.6f %-6d %-16s %1s %s" % (
             time.time() - start_ts, event.pid,
             event.comm.decode('utf-8', 'replace'), mode_s[event.type],
