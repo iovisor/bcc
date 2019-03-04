151e9b2f4c29ad934c1612bcffa9ca67e74722ef
Zwb <ethercflow@gmail.com>
2019-03-04 15:54:22 +0800

    guard the kernel version to make drsnoop smoke test pass

--- tests/python/test_tools_smoke.py
+++ tests/python/test_tools_smoke.py
@@ -144,6 +144,7 @@ class SmokeTests(TestCase):
         # self.run_with_int("deadlock.py $(pgrep -n bash)", timeout=10)
         pass
 
+    @skipUnless(kernel_version_ge(4,7), "requires kernel >= 4.7")
     def test_drsnoop(self):
         self.run_with_int("drsnoop.py")
 
