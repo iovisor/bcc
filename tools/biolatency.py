c6cded439f77d345d2ff132798f8dde00dbdc60a
Brendan Gregg <brendan.d.gregg@gmail.com>
2019-03-20 09:32:20 -0700

    biolatency: add -F for I/O flags (#2280)
    biolatency: add -F for I/O flags

--- tools/biolatency.py
+++ tools/biolatency.py
@@ -23,6 +23,7 @@ examples = """examples:
     ./biolatency -mT 1      # 1s summaries, milliseconds, and timestamps
     ./biolatency -Q         # include OS queued time in I/O time
     ./biolatency -D         # show each disk device separately
+    ./biolatency -F         # show I/O flags separately
 """
 parser = argparse.ArgumentParser(
     description="Summarize block device I/O latency as a histogram",
@@ -36,6 +37,8 @@ parser.add_argument("-m", "--milliseconds", action="store_true",
     help="millisecond histogram")
 parser.add_argument("-D", "--disks", action="store_true",
     help="print a histogram per disk device")
+parser.add_argument("-F", "--flags", action="store_true",
+    help="print a histogram per set of I/O flags")
 parser.add_argument("interval", nargs="?", default=99999999,
     help="output interval, in seconds")
 parser.add_argument("count", nargs="?", default=99999999,
@@ -45,6 +48,9 @@ parser.add_argument("--ebpf", action="store_true",
 args = parser.parse_args()
 countdown = int(args.count)
 debug = 0
+if args.flags and args.disks:
+    print("ERROR: can only use -D or -F. Exiting.")
+    exit()
 
 # define BPF program
 bpf_text = """
@@ -55,6 +61,12 @@ typedef struct disk_key {
     char disk[DISK_NAME_LEN];
     u64 slot;
 } disk_key_t;
+
+typedef struct flag_key {
+    u64 flags;
+    u64 slot;
+} flag_key_t;
+
 BPF_HASH(start, struct request *);
 STORAGE
 
@@ -102,6 +114,13 @@ if args.disks:
         'void *__tmp = (void *)req->rq_disk->disk_name; ' +
         'bpf_probe_read(&key.disk, sizeof(key.disk), __tmp); ' +
         'dist.increment(key);')
+elif args.flags:
+    bpf_text = bpf_text.replace('STORAGE',
+        'BPF_HISTOGRAM(dist, flag_key_t);')
+    bpf_text = bpf_text.replace('STORE',
+        'flag_key_t key = {.slot = bpf_log2l(delta)}; ' +
+        'key.flags = req->cmd_flags; ' +
+        'dist.increment(key);')
 else:
     bpf_text = bpf_text.replace('STORAGE', 'BPF_HISTOGRAM(dist);')
     bpf_text = bpf_text.replace('STORE',
@@ -124,6 +143,56 @@ b.attach_kprobe(event="blk_account_io_done",
 
 print("Tracing block device I/O... Hit Ctrl-C to end.")
 
+# see blk_fill_rwbs():
+req_opf = {
+    0: "Read",
+    1: "Write",
+    2: "Flush",
+    3: "Discard",
+    5: "SecureErase",
+    6: "ZoneReset",
+    7: "WriteSame",
+    9: "WriteZeros"
+}
+REQ_OP_BITS = 8
+REQ_OP_MASK = ((1 << REQ_OP_BITS) - 1)
+REQ_SYNC = 1 << (REQ_OP_BITS + 3)
+REQ_META = 1 << (REQ_OP_BITS + 4)
+REQ_PRIO = 1 << (REQ_OP_BITS + 5)
+REQ_NOMERGE = 1 << (REQ_OP_BITS + 6)
+REQ_IDLE = 1 << (REQ_OP_BITS + 7)
+REQ_FUA = 1 << (REQ_OP_BITS + 9)
+REQ_RAHEAD = 1 << (REQ_OP_BITS + 11)
+REQ_BACKGROUND = 1 << (REQ_OP_BITS + 12)
+REQ_NOWAIT = 1 << (REQ_OP_BITS + 13)
+def flags_print(flags):
+    desc = ""
+    # operation
+    if flags & REQ_OP_MASK in req_opf:
+        desc = req_opf[flags & REQ_OP_MASK]
+    else:
+        desc = "Unknown"
+    # flags
+    if flags & REQ_SYNC:
+        desc = "Sync-" + desc
+    if flags & REQ_META:
+        desc = "Metadata-" + desc
+    if flags & REQ_FUA:
+        desc = "ForcedUnitAccess-" + desc
+    if flags & REQ_PRIO:
+        desc = "Priority-" + desc
+    if flags & REQ_NOMERGE:
+        desc = "NoMerge-" + desc
+    if flags & REQ_IDLE:
+        desc = "Idle-" + desc
+    if flags & REQ_RAHEAD:
+        desc = "ReadAhead-" + desc
+    if flags & REQ_BACKGROUND:
+        desc = "Background-" + desc
+    if flags & REQ_NOWAIT:
+        desc = "NoWait-" + desc
+    return desc
+
 # output
 exiting = 0 if args.interval else 1
 dist = b.get_table("dist")
@@ -137,7 +206,10 @@ while (1):
     if args.timestamp:
         print("%-8s\n" % strftime("%H:%M:%S"), end="")
 
-    dist.print_log2_hist(label, "disk")
+    if args.flags:
+        dist.print_log2_hist(label, "flags", flags_print)
+    else:
+        dist.print_log2_hist(label, "disk")
     dist.clear()
 
     countdown -= 1
