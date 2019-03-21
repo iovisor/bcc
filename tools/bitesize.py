6b05e616464715e0e152d84b52ecdbc1bfccb76b
Brendan Gregg <brendan.d.gregg@gmail.com>
2019-03-20 21:13:59 -0700

    bitesize: switch to issue tracepoint (#2283)
    bitesize: switch to trace_block_rq_issue tracepoint

--- tools/bitesize.py
+++ tools/bitesize.py
@@ -27,7 +27,7 @@ struct proc_key_t {
 
 BPF_HISTOGRAM(dist, struct proc_key_t);
 
-TRACEPOINT_PROBE(block, block_rq_insert)
+TRACEPOINT_PROBE(block, block_rq_issue)
 {
     struct proc_key_t key = {.slot = bpf_log2l(args->bytes / 1024)};
     bpf_probe_read(&key.name, sizeof(key.name), args->comm);
