3df3879b189363af3294217af1dbc2f70f87ca11
cclauss <cclauss@me.com>
2019-07-04 16:29:37 +0200

    py-style-check.sh; Upgrade from pep8 to flake8

--- scripts/py-style-check.sh
+++ scripts/py-style-check.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 # TODO: stop ignoring this. Maybe autopep8 existing stuff?
-find tools -type f -name "*.py" | xargs pep8 -r --show-source --ignore=E123,E125,E126,E127,E128,E302 || \
-    echo "pep8 run failed, please fix it" >&2
+find tools -type f -name "*.py" | xargs flake8 || echo "flake8 run failed, please fix it" >&2
+flake8 . || echo "flake8 run failed, please fix it" >&2
 
 NO_PROPER_SHEBANG="$(find tools examples -type f -executable -name '*.py' | xargs grep -L '#!/usr/bin/python')"
 if [ -n "$NO_PROPER_SHEBANG" ]; then
