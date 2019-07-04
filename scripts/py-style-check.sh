45fd2f727864c6690def73046ce4e102d77ca6e1
cclauss <cclauss@me.com>
2019-07-04 22:14:26 +0200

    Upgrade from pep8 --> pycodestyle

--- scripts/py-style-check.sh
+++ scripts/py-style-check.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 # TODO: stop ignoring this. Maybe autopep8 existing stuff?
-find tools -type f -name "*.py" | xargs pep8 -r --show-source --ignore=E123,E125,E126,E127,E128,E302 || \
-    echo "pep8 run failed, please fix it" >&2
+find tools -type f -name "*.py" | xargs pycodestyle -r --show-source --ignore=E123,E125,E126,E127,E128,E302 || \
+    echo "pycodestyle run failed, please fix it" >&2
 
 NO_PROPER_SHEBANG="$(find tools examples -type f -executable -name '*.py' | xargs grep -L '#!/usr/bin/python')"
 if [ -n "$NO_PROPER_SHEBANG" ]; then
