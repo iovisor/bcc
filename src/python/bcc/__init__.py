ad91c91054e7c4cf59bc28ba7c7c6933d491ebdd
cclauss <cclauss@me.com>
2019-07-04 22:48:45 +0200

    Avoid shadowing variable "name"

--- src/python/bcc/__init__.py
+++ src/python/bcc/__init__.py
@@ -929,7 +929,7 @@ class BPF(object):
 
     @staticmethod
     def get_user_functions(name, sym_re):
-        return set([name for (name, _) in
+        return set([name_ for (name_, _) in
                     BPF.get_user_functions_and_addresses(name, sym_re)])
 
     @staticmethod
