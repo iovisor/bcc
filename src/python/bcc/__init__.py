44454d948b9fa4e0587fad7478b9383d51501c41
Michał Wesołowski <michal.wesolowski@horus.pl>
2019-09-11 11:08:58 +0200

    support gcc 8's cold subfunctions

--- src/python/bcc/__init__.py
+++ src/python/bcc/__init__.py
@@ -576,7 +576,7 @@ class BPF(object):
                 elif fn.startswith(b'__perf') or fn.startswith(b'perf_'):
                     continue
                 # Exclude all gcc 8's extra .cold functions
-                elif re.match(b'^.*\.cold\.\d+$', fn):
+                elif re.match(b'^.*\.cold(\.\d+)?$', fn):
                     continue
                 if (t.lower() in [b't', b'w']) and re.match(event_re, fn) \
                     and fn not in blacklist:
