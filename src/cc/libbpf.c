e66fbf02dc6365ba1565b864af831fcfd81540e5
Yonghong Song <yhs@fb.com>
2019-10-02 16:05:15 -0700

    sync to latest libbpf
    sync to latest libpf with tag v0.0.5.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>

--- src/cc/libbpf.c
+++ src/cc/libbpf.c
@@ -207,6 +207,7 @@ static struct bpf_helper helpers[] = {
   {"sk_storage_get", "5.2"},
   {"sk_storage_delete", "5.2"},
   {"send_signal", "5.3"},
+  {"tcp_gen_syncookie", "5.3"},
 };
 
 static uint64_t ptr_to_u64(void *ptr)
