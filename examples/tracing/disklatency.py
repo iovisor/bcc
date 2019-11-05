ecd53517a575a50d532d99777fec8cae8d75a650
jason-chenyixi-chengdu <414477685@qq.com>
2019-11-05 18:57:53 +0800

    add enhance, wait testing

---
+++ examples/tracing/disklatency.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#
+# disksnoop.py	Trace block device I/O: basic version of iosnoop.
+#		For Linux, uses BCC, eBPF. Embedded C.
+#
+# Written as a basic example of tracing latency.
+#
+# Copyright (c) 2015 Brendan Gregg.
+# Licensed under the Apache License, Version 2.0 (the "License")
+#
+# 11-Aug-2015	Brendan Gregg	Created this.
+
+from __future__ import print_function
+from bcc import BPF
+from bcc.utils import printb
+from time import sleep
+
+
+# load BPF program
+b = BPF(text="""
+#include <uapi/linux/ptrace.h>
+#include <linux/blkdev.h>
+
+BPF_HASH(start, struct request *);
+BPF_HISTOGRAM(iolatency);
+
+void trace_start(struct pt_regs *ctx, struct request *req) {
+	// stash start timestamp by request ptr
+	u64 ts = bpf_ktime_get_ns();
+	start.update(&req, &ts);
+}
+
+void trace_completion(struct pt_regs *ctx, struct request *req) {
+	u64 *tsp, delta;
+
+	tsp = start.lookup(&req);
+	if (tsp != 0)
+		delta = bpf_ktime_get_ns() - *tsp;
+    iolatency.increment(bpf_log2l(delta));
+    start.delete(&req);
+}
+
+
+""")
+
+if BPF.get_kprobe_functions(b'blk_start_request'):
+        b.attach_kprobe(event="blk_start_request", fn_name="trace_start")
+b.attach_kprobe(event="blk_mq_start_request", fn_name="trace_start")
+b.attach_kprobe(event="blk_account_io_completion", fn_name="trace_completion")
+
+# header
+print("Tracing... Hit Ctrl-C to end.")
+
+try:
+	sleep(99999999)
+except KeyboardInterrupt:
+	print()
+
+# output
+print("log2 histogram")
+print("~~~~~~~~~~~~~~")
+b["iolatency"].print_log2_hist("BYTES")
+
+
