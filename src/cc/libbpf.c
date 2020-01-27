83b67c2a05b107ffc27cb9b8cec0bec9f5c43a84
Yonghong Song <yhs@fb.com>
2020-01-27 14:35:21 -0800

    sync to latest libbpf repo
    Sync to latest libbpf repo. New helper bpf_jiffies64
    and new program type BPF_PROG_TYPE_EXT is added.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>

--- src/cc/libbpf.c
+++ src/cc/libbpf.c
@@ -215,6 +215,7 @@ static struct bpf_helper helpers[] = {
   {"probe_read_kernel_str", "5.5"},
   {"tcp_send_ack", "5.5"},
   {"send_signal_thread", "5.5"},
+  {"jiffies64", "5.5"},
 };
 
 static uint64_t ptr_to_u64(void *ptr)
