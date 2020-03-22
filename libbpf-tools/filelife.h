a523efdd2c00b77c446bc62481a7993b328e5fd8
Wenbo Zhang <ethercflow@gmail.com>
2020-03-21 22:59:43 -0400

    libbpf-tools: add CO-RE filelife
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

---
+++ libbpf-tools/filelife.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#ifndef __FILELIFE_H
+#define __FILELIFE_H
+
+#define DNAME_INLINE_LEN 32
+#define TASK_COMM_LEN    16
+
+struct event {
+	char file[DNAME_INLINE_LEN];
+	char task[TASK_COMM_LEN];
+	__u64 delta_ns;
+	pid_t tgid;
+};
+
+#endif /* __FILELIFE_H */
