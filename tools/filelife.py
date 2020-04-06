15d7955e07a942340610f71e44f44046b960e3a8
DavadDi <dwh0403@163.com>
2020-04-06 20:22:29 +0800

    Update pid filter
    Update pid filter;  u32 pid = bpf_get_current_pid_tgid() >> 32;

--- tools/filelife.py
+++ tools/filelife.py
@@ -57,7 +57,7 @@ BPF_PERF_OUTPUT(events);
 // trace file creation time
 int trace_create(struct pt_regs *ctx, struct inode *dir, struct dentry *dentry)
 {
-    u32 pid = bpf_get_current_pid_tgid();
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
     FILTER
 
     u64 ts = bpf_ktime_get_ns();
@@ -70,7 +70,7 @@ int trace_create(struct pt_regs *ctx, struct inode *dir, struct dentry *dentry)
 int trace_unlink(struct pt_regs *ctx, struct inode *dir, struct dentry *dentry)
 {
     struct data_t data = {};
-    u32 pid = bpf_get_current_pid_tgid();
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
 
     FILTER
 
