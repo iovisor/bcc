05f3f8668481cf975cbf89338e0db7d411ef1432
Andrii Nakryiko <andriin@fb.com>
2020-06-20 20:01:52 -0700

    libbpf-tools: remove unnecessary header include from syscount.bpf.c
    It causes build failure on my system due to trying to include GCC-specific
    header. It doesn't seem to be necessary, though, so remove it.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

--- libbpf-tools/syscount.bpf.c
+++ libbpf-tools/syscount.bpf.c
@@ -6,7 +6,6 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
-#include <string.h>
 #include "syscount.h"
 
 const volatile bool count_by_process = false;
