7c92acc98f1a977ee50b5c867a2fd7c85e999e4e
William Findlay <william@williamfindlay.com>
2020-06-24 21:01:35 -0400

    Add ringbuf tests file

---
+++ tests/python/test_ringbuf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Copyright (c) PLUMgrid, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License")
+
+from bcc import BPF
+import ctypes as ct
+import random
+import time
+import subprocess
+from unittest import main, TestCase
+
+class TestRingbuf(TestCase):
+    def test_ringbuf_output(self):
+        self.counter = 0
+
+        class Data(ct.Structure):
+            _fields_ = [("ts", ct.c_ulonglong)]
+
+        def cb(ctx, data, size):
+            self.assertEqual(size, ct.sizeof(Data))
+            event = ct.cast(data, ct.POINTER(Data)).contents
+            self.counter += 1
+
+        text = """
+BPF_RINGBUF_OUTPUT(events, 8);
+struct data_t {
+    u64 ts;
+};
+int do_sys_nanosleep(void *ctx) {
+    struct data_t data = {bpf_ktime_get_ns()};
+    events.ringbuf_output(&data, sizeof(data), 0);
+    return 0;
+}
+"""
+        b = BPF(text=text)
+        b.attach_kprobe(event=b.get_syscall_fnname("nanosleep"),
+                        fn_name="do_sys_nanosleep")
+        b.attach_kprobe(event=b.get_syscall_fnname("clock_nanosleep"),
+                        fn_name="do_sys_nanosleep")
+        b["events"].open_ring_buffer(cb)
+        subprocess.call(['sleep', '0.1'])
+        b.ring_buffer_poll(10)
+        self.assertGreater(self.counter, 0)
+        b.cleanup()
+
+if __name__ == "__main__":
+    main()
