0e7304eab277320bf0b0014a3940541aadea03e0
Kenta Tada <Kenta.Tada@sony.com>
2020-09-01 18:15:41 +0900

    tools/capable: support new capabilities
    This commit adds the support of below capabilities.
    * CAP_PERFMON
    * CAP_BPF
    * CAP_CHECKPOINT_RESTORE
    
    Signed-off-by: Kenta Tada <Kenta.Tada@sony.com>

--- tools/capable.py
+++ tools/capable.py
@@ -97,6 +97,9 @@ capabilities = {
     35: "CAP_WAKE_ALARM",
     36: "CAP_BLOCK_SUSPEND",
     37: "CAP_AUDIT_READ",
+    38: "CAP_PERFMON",
+    39: "CAP_BPF",
+    40: "CAP_CHECKPOINT_RESTORE",
 }
 
 class Enum(set):
