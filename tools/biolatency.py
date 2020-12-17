165137763fd3fa801b03d1d4e66445dca9b47274
Omkar Desai <omkarbdesai@gmail.com>
2020-12-17 17:25:30 -0500

    add some examples

--- tools/biolatency.py
+++ tools/biolatency.py
@@ -20,13 +20,14 @@ import os.path
 
 # arguments
 examples = """examples:
-    ./biolatency            # summarize block I/O latency as a histogram
-    ./biolatency 1 10       # print 1 second summaries, 10 times
-    ./biolatency -mT 1      # 1s summaries, milliseconds, and timestamps
-    ./biolatency -Q         # include OS queued time in I/O time
-    ./biolatency -D         # show each disk device separately
-    ./biolatency -F         # show I/O flags separately
-    ./biolatency -j         # output a dict                                 # github.com/swiftomkar
+    ./biolatency                    # summarize block I/O latency as a histogram
+    ./biolatency 1 10               # print 1 second summaries, 10 times
+    ./biolatency -mT 1              # 1s summaries, milliseconds, and timestamps
+    ./biolatency -Q                 # include OS queued time in I/O time
+    ./biolatency -D                 # show each disk device separately
+    ./biolatency -F                 # show I/O flags separately
+    ./biolatency -j                 # print a dictionary
+    ./biolatency -j -f name.json    # Dump the histogram dictionary to name.json file
 """
 parser = argparse.ArgumentParser(
     description="Summarize block device I/O latency as a histogram",
