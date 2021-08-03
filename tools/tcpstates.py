2ec3c8600d22bd6eac628615ba0ea6e2f7babc0b
rosenlo <rosenluov@gmail.com>
2021-08-03 11:35:40 +0800

    tcpstates: ntohs dport

--- tools/tcpstates.py
+++ tools/tcpstates.py
@@ -117,6 +117,7 @@ TRACEPOINT_PROBE(sock, inet_sock_set_state)
 
     // dport is either used in a filter here, or later
     u16 dport = args->dport;
+    dport = ntohs(dport);
     FILTER_DPORT
 
     // calculate delta
