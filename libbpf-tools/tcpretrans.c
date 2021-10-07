0fc4d2b5253e6db85e93ad0f595804f611cbfad9
Michael Gugino <gugino.michael@yahoo.com>
2021-10-07 09:35:53 -0400

    fix error string
    Co-authored-by: Mauricio Vásquez <mauricio@kinvolk.io>

--- libbpf-tools/tcpretrans.c
+++ libbpf-tools/tcpretrans.c
@@ -345,7 +345,7 @@ int main(int argc, char **argv)
 	}
 
 	if (signal(SIGINT, sig_handler) == SIG_ERR || signal(SIGTERM, sig_handler) == SIG_ERR) {
-		warn("can't set signal handler: %s\n", strerror(-errno));
+		warn("can't set signal handler: %s\n", strerror(errno));
 		err = 1;
 		goto cleanup;
 	}
