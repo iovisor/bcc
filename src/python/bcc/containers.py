iovisor/bcc

commit 284ca58d1965d4634e40bc4b5e591a04eb5f8b4e

tools/containers: Add print_container_info().
If --containersmap is used, this function will print container node, namespace,
pod and name to standard output without a new line.

Tools were updated to use it.

Signed-off-by: Francis Laniel <flaniel@microsoft.com>

src/python/bcc/containers.py
@@ -191,3 +191,17 @@ class ContainersMap:
         eventJ["pod"] = container.PodName
         eventJ["container"] = container.ContainerName
         eventJ["namespace"] = container.Namespace
+
+    def print_container_info(self, mntnsid):
+        """
+        Print container information like its node, namespace, pod and name.
+
+        If no container exists for the given argument, "<>" will be printed
+        instead of the information.
+
+        :param mntnsid: The mount namespace identifier of the container we want
+        to print information.
+        """
+        container = self.get_container(mntnsid)
+
+        print("{:16} {:16} {:16} {:16}".format(container.NodeName, container.Namespace, container.PodName, container.ContainerName), end = '')

tools/bindsnoop.py
@@ -465,8 +465,7 @@ def print_ipv4_bind_event(cpu, data, size):
     event = b["ipv4_bind_events"].event(data)
     global start_ts
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        printb("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), nl="")
+        containers_map.print_container_info(event.mntnsid)
     if args.timestamp:
         if start_ts == 0:
             start_ts = event.ts_us
@@ -490,8 +489,7 @@ def print_ipv6_bind_event(cpu, data, size):
     event = b["ipv6_bind_events"].event(data)
     global start_ts
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        printb("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), nl="")
+        containers_map.print_container_info(event.mntnsid)
     if args.timestamp:
         if start_ts == 0:
             start_ts = event.ts_us

tools/capable.py
@@ -297,8 +297,7 @@ def print_event(bpf, cpu, data, size):
     else:
         name = "?"
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        print("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), end="")
+        containers_map.print_container_info(event.mntnsid)
 
     if args.extra:
         print("%-9s %-6d %-6d %-6d %-16s %-4d %-20s %-6d %s" % (strftime("%H:%M:%S"),

tools/execsnoop.py
@@ -308,8 +308,7 @@ def print_event(cpu, data, size):
 
         if not skip:
             if args.containersmap:
-                container = containers_map.get_container(event.mntnsid)
-                printb("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), nl="")
+                containers_map.print_container_info(event.mntnsid)
             if args.time:
                 printb(b"%-9s" % strftime("%H:%M:%S").encode('ascii'), nl="")
             if args.timestamp:

tools/opensnoop.py
@@ -394,8 +394,7 @@ def print_event(cpu, data, size):
         return
 
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        printb("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), nl="")
+        containers_map.print_container_info(event.mntnsid)
 
     if args.timestamp:
         delta = event.ts - initial_ts

tools/tcpconnect.py
@@ -422,8 +422,7 @@ def print_ipv4_event(cpu, data, size):
     event = b["ipv4_events"].event(data)
     global start_ts
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        printb("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), nl="")
+        containers_map.print_container_info(event.mntnsid)
     if args.timestamp:
         if start_ts == 0:
             start_ts = event.ts_us
@@ -449,8 +448,7 @@ def print_ipv6_event(cpu, data, size):
     event = b["ipv6_events"].event(data)
     global start_ts
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        printb("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), nl="")
+        containers_map.print_container_info(event.mntnsid)
     if args.timestamp:
         if start_ts == 0:
             start_ts = event.ts_us

tools/tcptracer.py
@@ -602,8 +602,7 @@ def print_ipv4_event(cpu, data, size):
     global start_ts
 
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        print("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), end="")
+        containers_map.print_container_info(event.mntnsid)
 
     if args.timestamp:
         if start_ts == 0:
@@ -646,8 +645,7 @@ def print_ipv6_event(cpu, data, size):
     event = b["tcp_ipv6_event"].event(data)
     global start_ts
     if args.containersmap:
-        container = containers_map.get_container(event.mntnsid)
-        print("%-16s %-16s %-16s %-16s" % (container.NodeName, container.Namespace, container.PodName, container.ContainerName), end="")
+        containers_map.print_container_info(event.mntnsid)
     if args.timestamp:
         if start_ts == 0:
             start_ts = event.ts_ns

commit 96c7212406956ea8f5668f15c32d4fe8769f8d5e

containers: Add space after container info and header printing.
This commit fixes previous commits by adding a space after printing container
information and header.
Thus, this behavior is fixed:
minikube         kube-system      mypodaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbb mypodaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbsh               5447   5438     0 /bin/sh

Fixes: 284ca58d1965 ("tools/containers: Add print_container_info().")
Fixes: 0db55450a6ea ("containers: Add print_container_info_header().")
Signed-off-by: Francis Laniel <flaniel@microsoft.com>

src/python/bcc/containers.py
@@ -140,7 +140,7 @@ POD_HEADER = "POD"
 CONTAINER_HEADER = "CONTAINER"
 
 def print_container_info_header():
-    print('{:16} {:16} {:16} {:16}'.format(NODE_HEADER, NAMESPACE_HEADER, POD_HEADER, CONTAINER_HEADER), end = '')
+    print('{:16} {:16} {:16} {:16} '.format(NODE_HEADER, NAMESPACE_HEADER, POD_HEADER, CONTAINER_HEADER), end = '')
 
 class ContainerC(ct.Structure):
     _fields_ = [
@@ -204,4 +204,4 @@ class ContainersMap:
         """
         container = self.get_container(mntnsid)
 
-        print("{:16} {:16} {:16} {:16}".format(container.NodeName, container.Namespace, container.PodName, container.ContainerName), end = '')
+        print("{:16} {:16} {:16} {:16} ".format(container.NodeName, container.Namespace, container.PodName, container.ContainerName), end = '')