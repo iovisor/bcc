66a86795000a913b787f7ae4cd7b9b1f464bdd5e
Barret Rhoden <brho@google.com>
2021-11-02 17:05:35 -0400

    libbpf-tools: add klockstat
    This is a port of BCC's klockstat.  Differences from BCC:
    - can specify a lock by ksym name, using -L
    - tracks whichever task had the max time for acquire and hold, outputted
    when -s > 1 (otherwise it's cluttered).
    - does not reset stats each interval by default.  Can request with -R.
    
    -------------
    Usage: klockstat [-hRT] [-p PID] [-t TID] [-c FUNC] [-L LOCK] [-n NR_LOCKS]
                     [-s NR_STACKS] [-S SORT] [-d DURATION] [-i INTERVAL]
    
      -p, --pid=PID              Filter by process ID
      -t, --tid=TID              Filter by thread ID
      -c, --caller=FUNC          Filter by caller string prefix
      -L, --lock=LOCK            Filter by specific ksym lock name
      -n, --locks=NR_LOCKS       Number of locks to print
      -s, --stacks=NR_STACKS     Number of stack entries to print per lock
      -S, --sort=SORT            Sort by field:
                                   acq_[max|total|count]
                                   hld_[max|total|count]
      -d, --duration=SECONDS     Duration to trace
      -i, --interval=SECONDS     Print interval
      -R, --reset                Reset stats each interval
      -T, --timestamp            Print timestamp
    
      -?, --help                 Give this help list
          --usage                Give a short usage message
      -V, --version              Print program version
    
    Mandatory or optional arguments to long options are also mandatory or optional
    for any corresponding short options.
    
    Examples:
      klockstat                     # trace system wide until ctrl-c
      klockstat -d 5                # trace for 5 seconds
      klockstat -i 5                # print stats every 5 seconds
      klockstat -p 181              # trace process 181 only
      klockstat -t 181              # trace thread 181 only
      klockstat -c pipe_            # print only for lock callers with 'pipe_'
                                    # prefix
      klockstat -L cgroup_mutex     # trace the cgroup_mutex lock only
      klockstat -S acq_count        # sort lock acquired results by acquire count
      klockstat -S hld_total        # sort lock held results by total held time
      klockstat -S acq_count,hld_total  # combination of above
      klockstat -n 3                # display top 3 locks
      klockstat -s 6                # display 6 stack entries per lock
    
    -------------
    
    Signed-off-by: Barret Rhoden <brho@google.com>

---
+++ libbpf-tools/klockstat.bpf.c
@@ -0,0 +1,230 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2021 Google LLC.
+ *
+ * Based on klockstat from BCC by Jiri Olsa and others
+ * 2021-10-26   Barret Rhoden   Created this.
+ */
+#include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include "klockstat.h"
+#include "bits.bpf.h"
+
+const volatile pid_t targ_tgid = 0;
+const volatile pid_t targ_pid = 0;
+struct mutex *const volatile targ_lock = NULL;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_STACK_TRACE);
+	__uint(max_entries, MAX_ENTRIES);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, PERF_MAX_STACK_DEPTH * sizeof(u64));
+} stack_map SEC(".maps");
+
+/*
+ * Uniquely identifies a task grabbing a particular lock; a task can only hold
+ * the same lock once (non-recursive mutexes).
+ */
+struct task_lock {
+	u64 task_id;
+	u64 lock_ptr;
+};
+
+struct lockholder_info {
+	s32 stack_id;
+	u64 task_id;
+	u64 try_at;
+	u64 acq_at;
+	u64 rel_at;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, struct task_lock);
+	__type(value, struct lockholder_info);
+} lockholder_map SEC(".maps");
+
+/*
+ * Keyed by stack_id.
+ *
+ * Multiple call sites may have the same underlying lock, but we only know the
+ * stats for a particular stack frame.  Multiple tasks may have the same
+ * stackframe.
+ */
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, s32);
+	__type(value, struct lock_stat);
+} stat_map SEC(".maps");
+
+static bool tracing_task(u64 task_id)
+{
+	u32 tgid = task_id >> 32;
+	u32 pid = task_id;
+
+	if (targ_tgid && targ_tgid != tgid)
+		return false;
+	if (targ_pid && targ_pid != pid)
+		return false;
+	return true;
+}
+
+static void lock_contended(void *ctx, struct mutex *lock)
+{
+	u64 task_id;
+	struct lockholder_info li[1] = {0};
+	struct task_lock tl = {};
+
+	if (targ_lock && targ_lock != lock)
+		return;
+	task_id = bpf_get_current_pid_tgid();
+	if (!tracing_task(task_id))
+		return;
+
+	li->task_id = task_id;
+	/*
+	 * Skip 4 frames, e.g.:
+	 *       __this_module+0x34ef
+	 *       __this_module+0x34ef
+	 *       __this_module+0x8c44
+	 *             mutex_lock+0x5
+	 *
+	 * Note: if you make major changes to this bpf program, double check
+	 * that you aren't skipping too many frames.
+	 */
+	li->stack_id = bpf_get_stackid(ctx, &stack_map,
+				       4 | BPF_F_FAST_STACK_CMP);
+	/* Legit failures include EEXIST */
+	if (li->stack_id < 0)
+		return;
+	li->try_at = bpf_ktime_get_ns();
+
+	tl.task_id = task_id;
+	tl.lock_ptr = (u64)lock;
+	bpf_map_update_elem(&lockholder_map, &tl, li, BPF_ANY);
+}
+
+static void lock_acquired(struct mutex *lock)
+{
+	u64 task_id;
+	struct lockholder_info *li;
+	struct task_lock tl = {};
+
+	if (targ_lock && targ_lock != lock)
+		return;
+	task_id = bpf_get_current_pid_tgid();
+	if (!tracing_task(task_id))
+		return;
+
+	tl.task_id = task_id;
+	tl.lock_ptr = (u64)lock;
+	li = bpf_map_lookup_elem(&lockholder_map, &tl);
+	if (!li)
+		return;
+
+	li->acq_at = bpf_ktime_get_ns();
+}
+
+static void account(struct lockholder_info *li)
+{
+	struct lock_stat *ls;
+	u64 delta;
+
+	/*
+	 * Multiple threads may have the same stack_id.  Even though we are
+	 * holding the lock, dynamically allocated mutexes can have the same
+	 * callgraph but represent different locks.  They will be accounted as
+	 * the same lock, which is what we want, but we need to use atomics to
+	 * avoid corruption, especially for the total_time variables.
+	 */
+	ls = bpf_map_lookup_elem(&stat_map, &li->stack_id);
+	if (!ls) {
+		struct lock_stat fresh = {0};
+
+		bpf_map_update_elem(&stat_map, &li->stack_id, &fresh, BPF_ANY);
+		ls = bpf_map_lookup_elem(&stat_map, &li->stack_id);
+		if (!ls)
+			return;
+	}
+
+	delta = li->acq_at - li->try_at;
+	__sync_fetch_and_add(&ls->acq_count, 1);
+	__sync_fetch_and_add(&ls->acq_total_time, delta);
+	if (delta > READ_ONCE(ls->acq_max_time)) {
+		WRITE_ONCE(ls->acq_max_time, delta);
+		WRITE_ONCE(ls->acq_max_id, li->task_id);
+		/*
+		 * Potentially racy, if multiple threads think they are the max,
+		 * so you may get a clobbered write.
+		 */
+		bpf_get_current_comm(ls->acq_max_comm, TASK_COMM_LEN);
+	}
+
+	delta = li->rel_at - li->acq_at;
+	__sync_fetch_and_add(&ls->hld_count, 1);
+	__sync_fetch_and_add(&ls->hld_total_time, delta);
+	if (delta > READ_ONCE(ls->hld_max_time)) {
+		WRITE_ONCE(ls->hld_max_time, delta);
+		WRITE_ONCE(ls->hld_max_id, li->task_id);
+		bpf_get_current_comm(ls->hld_max_comm, TASK_COMM_LEN);
+	}
+}
+
+static void lock_released(struct mutex *lock)
+{
+	u64 task_id;
+	struct lockholder_info *li;
+	struct task_lock tl = {};
+
+	if (targ_lock && targ_lock != lock)
+		return;
+	task_id = bpf_get_current_pid_tgid();
+	if (!tracing_task(task_id))
+		return;
+	tl.task_id = task_id;
+	tl.lock_ptr = (u64)lock;
+	li = bpf_map_lookup_elem(&lockholder_map, &tl);
+	if (!li)
+		return;
+
+	li->rel_at = bpf_ktime_get_ns();
+	account(li);
+
+	bpf_map_delete_elem(&lockholder_map, &tl);
+}
+
+SEC("fentry/mutex_lock")
+int BPF_PROG(mutex_lock, struct mutex *lock)
+{
+	lock_contended(ctx, lock);
+	return 0;
+}
+
+SEC("fexit/mutex_lock")
+int BPF_PROG(mutex_lock_exit, struct mutex *lock, long ret)
+{
+	lock_acquired(lock);
+	return 0;
+}
+
+SEC("fexit/mutex_trylock")
+int BPF_PROG(mutex_trylock_exit, struct mutex *lock, long ret)
+{
+	if (ret) {
+		lock_contended(ctx, lock);
+		lock_acquired(lock);
+	}
+	return 0;
+}
+
+SEC("fentry/mutex_unlock")
+int BPF_PROG(mutex_unlock, struct mutex *lock)
+{
+	lock_released(lock);
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
---
+++ libbpf-tools/klockstat.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#ifndef __KLOCKSTAT_H
+
+#define MAX_ENTRIES 102400
+#define TASK_COMM_LEN 16
+#define PERF_MAX_STACK_DEPTH 127
+
+struct lock_stat {
+	__u64 acq_count;
+	__u64 acq_total_time;
+	__u64 acq_max_time;
+	__u64 acq_max_id;
+	char acq_max_comm[TASK_COMM_LEN];
+	__u64 hld_count;
+	__u64 hld_total_time;
+	__u64 hld_max_time;
+	__u64 hld_max_id;
+	char hld_max_comm[TASK_COMM_LEN];
+};
+
+#endif /*__KLOCKSTAT_H */
