dab1d09ef49e74c4c95f933e97e16dfc074338c2
Andrii Nakryiko <andrii@kernel.org>
2021-12-17 14:21:44 -0800

    libbpf-tools: update cpudist for libbpf 1.0
    Switch to libbpf 1.0 mode and adapt libbpf API usage accordingly.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- libbpf-tools/cpudist.c
+++ libbpf-tools/cpudist.c
@@ -198,13 +198,8 @@ int main(int argc, char **argv)
 	if (err)
 		return err;
 
+	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
 	libbpf_set_print(libbpf_print_fn);
-
-	err = bump_memlock_rlimit();
-	if (err) {
-		fprintf(stderr, "failed to increase rlimit: %d\n", err);
-		return 1;
-	}
 
 	obj = cpudist_bpf__open();
 	if (!obj) {
