ebd62dc49d1b31a4603f01a7f6742c5d7822b702
Rong Tao <32674962+Rtoax@users.noreply.github.com>
2022-01-06 20:32:06 +0800

    Update pagefaultsnoop.h

--- libbpf-tools/pagefaultsnoop.h
+++ libbpf-tools/pagefaultsnoop.h
@@ -3,6 +3,7 @@
 #define __PAGEFAULTSNOOP_H
 
 #define TASK_COMM_LEN	16
+#define MAX_SLOTS   32
 
 typedef enum {
 	PF_TYPE_FILE,
@@ -10,6 +11,7 @@ typedef enum {
 	PF_TYPE_SWAP,
 	PF_TYPE_NUMA,
 	PF_TYPE_WRITE,
+	PF_TYPE_MAX,
 } pf_type_enum;
 
 struct pagefault_event {
@@ -22,6 +24,11 @@ struct pagefault_event {
 	pf_type_enum pf_type;
 	char task[TASK_COMM_LEN];
 	unsigned long address;
+	__s64 delta;
+};
+
+struct hist {
+    __u32 slots[MAX_SLOTS];
 };
 
 #endif /* __PAGEFAULTSNOOP_H */
