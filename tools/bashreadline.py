0064febd47972cc6c79de64639790f5889387d03
xingfeng2510 <xingfeng25100@163.com>
2022-04-28 19:21:40 +0800

    tools: Use u32 as pid field type

--- tools/bashreadline.py
+++ tools/bashreadline.py
@@ -38,7 +38,7 @@ bpf_text = """
 #include <linux/sched.h>
 
 struct str_t {
-    u64 pid;
+    u32 pid;
     char str[80];
 };
 
@@ -47,11 +47,9 @@ BPF_PERF_OUTPUT(events);
 int printret(struct pt_regs *ctx) {
     struct str_t data  = {};
     char comm[TASK_COMM_LEN] = {};
-    u32 pid;
     if (!PT_REGS_RC(ctx))
         return 0;
-    pid = bpf_get_current_pid_tgid() >> 32;
-    data.pid = pid;
+    data.pid = bpf_get_current_pid_tgid() >> 32;
     bpf_probe_read_user(&data.str, sizeof(data.str), (void *)PT_REGS_RC(ctx));
 
     bpf_get_current_comm(&comm, sizeof(comm));
