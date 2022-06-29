7153330f395e98698b58f7cf93ac3c350da149e5
xingfeng2510 <xingfeng25100@163.com>
2022-06-29 23:48:31 +0800

    tools/biotop: Fix output format specifier

--- tools/biotop.py
+++ tools/biotop.py
@@ -259,7 +259,7 @@ while 1:
 
         # print line
         avg_ms = (float(v.us) / 1000) / v.io
-        print("%-7d %-16s %1s %-3d %-3d %-8s %5s %7s %6.2f" % (k.pid,
+        print("%-7d %-16s %1s %-3d %-3d %-8s %5s %7d %6.2f" % (k.pid,
             k.name.decode('utf-8', 'replace'), "W" if k.rwflag else "R",
             k.major, k.minor, diskname, v.io, v.bytes / 1024, avg_ms))
 
