535ad2e6a8b06b2b650366e5afeb030098a9a1fa
Curu Wong <prinbra@gmail.com>
2022-11-18 19:19:18 +0800

    tools/filevanish: some code adjust as cr suggestion from chenhengqi

--- tools/filevanish.py
+++ tools/filevanish.py
@@ -45,30 +45,29 @@ struct data_t {
     char fname2[DNAME_INLINE_LEN];
 };
 
-BPF_HASH(birth, struct dentry *);
 BPF_PERF_OUTPUT(events);
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)
 // trace file deletion and output details
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)
 int trace_unlink(struct pt_regs *ctx, struct inode *dir, struct dentry *dentry)
 #else
 int trace_unlink(struct pt_regs *ctx, struct user_namespace *ns, struct inode *dir, struct dentry *dentry)
 #endif
 {
-    struct data_t data = {};
-    u32 pid = bpf_get_current_pid_tgid();
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
 
     FILTER
 
+    struct data_t data = {};
     struct qstr d_name = dentry->d_name;
     if (d_name.len == 0)
         return 0;
 
-    if (bpf_get_current_comm(&data.comm, sizeof(data.comm)) == 0) {
-        data.pid = pid;
-        data.action = 'D';
-        bpf_probe_read(&data.fname, sizeof(data.fname), d_name.name);
-    }
+    bpf_get_current_comm(&data.comm, sizeof(data.comm));
+    data.pid = pid;
+    data.action = 'D';
+    bpf_probe_read(&data.fname, sizeof(data.fname), d_name.name);
+
     events.perf_submit(ctx, &data, sizeof(data));
 
     return 0;
@@ -76,7 +75,7 @@ int trace_unlink(struct pt_regs *ctx, struct user_namespace *ns, struct inode *d
 
 // trace file rename
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)
-int trace_rename(struct pt_regs *ctx, struct inode *old_dir, struct dentry *old_dentry, 
+int trace_rename(struct pt_regs *ctx, struct inode *old_dir, struct dentry *old_dentry,
 struct inode *new_dir, struct dentry *new_dentry)
 {
 #else
@@ -85,23 +84,23 @@ int trace_rename(struct pt_regs *ctx, struct renamedata *rd)
     struct dentry *old_dentry = rd->old_dentry;
     struct dentry *new_dentry = rd->new_dentry;
 #endif
-    struct data_t data = {};
-    u32 pid = bpf_get_current_pid_tgid();
+
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
 
     FILTER
 
+    struct data_t data = {};
     struct qstr s_name = old_dentry->d_name;
     struct qstr d_name = new_dentry->d_name;
-    if (d_name.len == 0)
+    if (s_name.len == 0 || d_name.len == 0)
         return 0;
 
-    if (bpf_get_current_comm(&data.comm, sizeof(data.comm)) == 0) {
-        data.pid = pid;
-        data.action = 'R';
-        bpf_probe_read(&data.fname, sizeof(data.fname), s_name.name);
-        bpf_probe_read(&data.fname2, sizeof(data.fname), d_name.name);
-        events.perf_submit(ctx, &data, sizeof(data));
-    }
+    bpf_get_current_comm(&data.comm, sizeof(data.comm));
+    data.pid = pid;
+    data.action = 'R';
+    bpf_probe_read(&data.fname, sizeof(data.fname), s_name.name);
+    bpf_probe_read(&data.fname2, sizeof(data.fname), d_name.name);
+    events.perf_submit(ctx, &data, sizeof(data));
 
     return 0;
 }
@@ -120,6 +119,7 @@ if args.pid:
         'if (pid != %s) { return 0; }' % args.pid)
 else:
     bpf_text = bpf_text.replace('FILTER', '')
+
 if debug or args.ebpf:
     print(bpf_text)
     if args.ebpf:
@@ -128,11 +128,13 @@ if debug or args.ebpf:
 # initialize BPF
 b = BPF(text=bpf_text)
 b.attach_kprobe(event="vfs_unlink", fn_name="trace_unlink")
+
+
 b.attach_kprobe(event="vfs_rmdir", fn_name="trace_unlink")
 b.attach_kprobe(event="vfs_rename", fn_name="trace_rename")
 
 # header
-print("%-8s %-7s %-16s %7s %s" % ("TIME", "PID", "COMM", "ACTION", "FILE"))
+print("%-8s %-7s %-16s %6s %s" % ("TIME", "PID", "COMM", "ACTION", "FILE"))
 
 # process event
 def print_event(cpu, data, size):
@@ -141,7 +143,7 @@ def print_event(cpu, data, size):
     file_str = event.fname.decode('utf-8', 'replace')
     if action_str == "RENAME":
         file_str = "%s > %s" % (file_str, event.fname2.decode('utf-8', 'replace'))
-    print("%-8s %-7d %-16s %7s %s" % (strftime("%H:%M:%S"), event.pid,
+    print("%-8s %-7d %-16s %6s %s" % (strftime("%H:%M:%S"), event.pid,
         event.comm.decode('utf-8', 'replace'), action_str, file_str))
 
 b["events"].open_perf_buffer(print_event)
