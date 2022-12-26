23bd97abf71f5746e8f0d5f8e66ccfd3ddfb0e92
Hengqi Chen <hengqi.chen@gmail.com>
2022-12-26 17:50:53 +0800

    libbpf-tools: Enable drsnoop on kernels without BPF trampoline
    Fallback to raw tracepoint if BPF trampoline not available.
    
    Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>

--- libbpf-tools/drsnoop.bpf.c
+++ libbpf-tools/drsnoop.bpf.c
@@ -27,8 +27,7 @@ struct {
 	__uint(value_size, sizeof(u32));
 } events SEC(".maps");
 
-SEC("tp_btf/mm_vmscan_direct_reclaim_begin")
-int BPF_PROG(direct_reclaim_begin)
+static int handle_direct_reclaim_begin()
 {
 	u64 *vm_zone_stat_kaddrp = (u64*)vm_zone_stat_kaddr;
 	u64 id = bpf_get_current_pid_tgid();
@@ -44,16 +43,15 @@ int BPF_PROG(direct_reclaim_begin)
 	piddata.ts = bpf_ktime_get_ns();
 	if (vm_zone_stat_kaddrp) {
 		bpf_probe_read_kernel(&piddata.nr_free_pages,
-			       sizeof(*vm_zone_stat_kaddrp),
-			       &vm_zone_stat_kaddrp[NR_FREE_PAGES]);
+				      sizeof(*vm_zone_stat_kaddrp),
+				      &vm_zone_stat_kaddrp[NR_FREE_PAGES]);
 	}
 
 	bpf_map_update_elem(&start, &pid, &piddata, 0);
 	return 0;
 }
 
-SEC("tp_btf/mm_vmscan_direct_reclaim_end")
-int BPF_PROG(direct_reclaim_end, unsigned long nr_reclaimed)
+static int handle_direct_reclaim_end(void *ctx, unsigned long nr_reclaimed)
 {
 	u64 id = bpf_get_current_pid_tgid();
 	struct piddata *piddatap;
@@ -89,6 +87,30 @@ int BPF_PROG(direct_reclaim_end, unsigned long nr_reclaimed)
 cleanup:
 	bpf_map_delete_elem(&start, &pid);
 	return 0;
+}
+
+SEC("tp_btf/mm_vmscan_direct_reclaim_begin")
+int BPF_PROG(direct_reclaim_begin_btf)
+{
+	return handle_direct_reclaim_begin();
+}
+
+SEC("tp_btf/mm_vmscan_direct_reclaim_end")
+int BPF_PROG(direct_reclaim_end_btf, unsigned long nr_reclaimed)
+{
+	return handle_direct_reclaim_end(ctx, nr_reclaimed);
+}
+
+SEC("raw_tp/mm_vmscan_direct_reclaim_begin")
+int BPF_PROG(direct_reclaim_begin)
+{
+	return handle_direct_reclaim_begin();
+}
+
+SEC("raw_tp/mm_vmscan_direct_reclaim_end")
+int BPF_PROG(direct_reclaim_end, unsigned long nr_reclaimed)
+{
+	return handle_direct_reclaim_end(ctx, nr_reclaimed);
 }
 
 char LICENSE[] SEC("license") = "GPL";
