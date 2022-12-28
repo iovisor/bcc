ea3a1e8f2075bfef0075dea07241b5fb6cca791a
inwardvessel <5782523+inwardvessel@users.noreply.github.com>
2022-12-27 16:32:17 -0800

    convert memleak to libbpf

---
+++ libbpf-tools/memleak.h
@@ -0,0 +1,21 @@
+#ifndef __MEMLEAK_H
+#define __MEMLEAK_H
+
+#define ALLOCS_MAX_ENTRIES 1000000
+#define COMBINED_ALLOCS_MAX_ENTRIES 10240
+
+struct alloc_info {
+	__u64 size;
+	__u64 timestamp_ns;
+	int stack_id;
+};
+
+union combined_alloc_info {
+	struct {
+		__u64 total_size : 40;
+		__u64 number_of_allocs : 24;
+	};
+	__u64 bits;
+};
+
+#endif /* __MEMLEAK_H */
