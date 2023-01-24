ca2b90a76d3811aa698c8273233e973d5833e0e4
Francis Laniel <flaniel@linux.microsoft.com>
2023-01-24 15:59:06 +0100

    golang: Extend copyright headers.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- src/golang/symbol.go
+++ src/golang/symbol.go
@@ -1,4 +1,5 @@
 // Copyright 2017 Louis McCormack
+// Copyright 2023 Microsoft
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
