20c6f84252d224ece9514fc8c3642cc091f5d1ea
Li Peng <pengli01@cestc.cn>
2023-04-03 19:21:23 +0800

    Fix typos in klockstat

--- tools/klockstat.py
+++ tools/klockstat.py
@@ -20,7 +20,7 @@ examples = """
     klockstat -d 5                      # trace for 5 seconds only
     klockstat -i 5                      # display stats every 5 seconds
     klockstat -p 123                    # trace locks for PID 123
-    klockstat -t 321                    # trace locks for PID 321
+    klockstat -t 321                    # trace locks for TID 321
     klockstat -c pipe_                  # display stats only for lock callers with 'pipe_' substring
     klockstat -S acq_count              # sort lock acquired results on acquired count
     klockstat -S hld_total              # sort lock held results on total held time
