575953e6cfa053dcbf0e1388c7254396f5bef3d7
Francis Laniel <flaniel@linux.microsoft.com>
2023-06-09 16:35:09 +0100

    libbpf: Refactor opensnoop.bpf.c
    Suggested-by: Jose Blanquicet <josebl@microsoft.com>
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- libbpf-tools/opensnoop.bpf.c
+++ libbpf-tools/opensnoop.bpf.c
@@ -46,8 +46,8 @@ bool trace_allowed(u32 tgid, u32 pid)
 	return true;
 }
 
-SEC("tracepoint/syscalls/sys_enter_open")
-int tracepoint__syscalls__sys_enter_open(struct trace_event_raw_sys_enter* ctx)
+static __always_inline
+int trace_enter(const char *filename, int flags)
 {
 	u64 id = bpf_get_current_pid_tgid();
 	/* use kernel terminology here for tgid/pid: */
@@ -57,29 +57,23 @@ int tracepoint__syscalls__sys_enter_open(struct trace_event_raw_sys_enter* ctx)
 	/* store arg info for later lookup */
 	if (trace_allowed(tgid, pid)) {
 		struct args_t args = {};
-		args.fname = (const char *)ctx->args[0];
-		args.flags = (int)ctx->args[1];
+		args.fname = filename;
+		args.flags = flags;
 		bpf_map_update_elem(&start, &pid, &args, 0);
 	}
 	return 0;
 }
 
+SEC("tracepoint/syscalls/sys_enter_open")
+int tracepoint__syscalls__sys_enter_open(struct trace_event_raw_sys_enter* ctx)
+{
+	return trace_enter((const char *)ctx->args[0], (int)ctx->args[1]);
+}
+
 SEC("tracepoint/syscalls/sys_enter_openat")
 int tracepoint__syscalls__sys_enter_openat(struct trace_event_raw_sys_enter* ctx)
 {
-	u64 id = bpf_get_current_pid_tgid();
-	/* use kernel terminology here for tgid/pid: */
-	u32 tgid = id >> 32;
-	u32 pid = id;
-
-	/* store arg info for later lookup */
-	if (trace_allowed(tgid, pid)) {
-		struct args_t args = {};
-		args.fname = (const char *)ctx->args[1];
-		args.flags = (int)ctx->args[2];
-		bpf_map_update_elem(&start, &pid, &args, 0);
-	}
-	return 0;
+	return trace_enter((const char *)ctx->args[1], (int)ctx->args[2]);
 }
 
 static __always_inline
