cce756c99df131a93795d5e094801c37e075211f
Konstantin Gukov <gukkos@gmail.com>
2024-05-23 17:52:13 +0200

    Extend `bcc_proc` API. Allow to limit search to specific pid.
    - Also extend the Python binding with the same goal.
    - The API changes are backwards-compatible.
    - Also added a couple of boundary checks for `memcpy`

--- tests/cc/test_c_api.cc
+++ tests/cc/test_c_api.cc
@@ -47,7 +47,15 @@ TEST_CASE("language detection", "[c_api]") {
 }
 
 TEST_CASE("shared object resolution", "[c_api]") {
-  char *libm = bcc_procutils_which_so("m", 0);
+  char *libm = bcc_procutils_which_so_in_ldconfig_cache("m");
+  REQUIRE(libm);
+  REQUIRE(libm[0] == '/');
+  REQUIRE(string(libm).find("libm.so") != string::npos);
+  free(libm);
+}
+
+TEST_CASE("shared object resolution with general function", "[c_api]") {
+  char *libm = bcc_procutils_which_so("m");
   REQUIRE(libm);
   REQUIRE(libm[0] == '/');
   REQUIRE(string(libm).find("libm.so") != string::npos);
@@ -55,6 +63,14 @@ TEST_CASE("shared object resolution", "[c_api]") {
 }
 
 TEST_CASE("shared object resolution using loaded libraries", "[c_api]") {
+  char *libelf = bcc_procutils_which_so_in_process("elf", getpid());
+  REQUIRE(libelf);
+  REQUIRE(libelf[0] == '/');
+  REQUIRE(string(libelf).find("libelf") != string::npos);
+  free(libelf);
+}
+
+TEST_CASE("shared object resolution using loaded libraries with general function", "[c_api]") {
   char *libelf = bcc_procutils_which_so("elf", getpid());
   REQUIRE(libelf);
   REQUIRE(libelf[0] == '/');
