2a7eec858456f0ff59f89aa14bf5bc53aa5c8e4e
Rong Tao <rongtao@cestc.cn>
2024-07-19 23:42:11 +0800

    libbpf-tools/syncsnoop: Add more syscalls support
    Add fsync(),fdatasync(),syncfs(),sync_file_range(),msync() syscalls to
    trace. For example:
    
        $ sudo ./syncsnoop
        TIME(s)            COMM             CALL
        7348.355957031     aria2c           fsync
        7350.032226562     ThreadPoolForeg  fdatasync
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Signed-off-by: Jiang Guirong <rtoax@foxmail.com>

--- libbpf-tools/syncsnoop.bpf.c
+++ libbpf-tools/syncsnoop.bpf.c
@@ -10,13 +10,52 @@ struct {
 	__uint(value_size, sizeof(u32));
 } events SEC(".maps");
 
-SEC("tracepoint/syscalls/sys_enter_sync")
-void tracepoint__syscalls__sys_enter_sync(struct trace_event_raw_sys_enter *ctx)
+
+static void __syscall(struct trace_event_raw_sys_enter *ctx,
+		      enum sync_syscalls sys)
 {
 	struct event event = {};
 
+	bpf_get_current_comm(event.comm, sizeof(event.comm));
 	event.ts_us = bpf_ktime_get_ns() / 1000;
+	event.sys = sys;
 	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+}
+
+SEC("tracepoint/syscalls/sys_enter_sync")
+void tracepoint__syscalls__sys_enter_sync(struct trace_event_raw_sys_enter *ctx)
+{
+	__syscall(ctx, SYS_SYNC);
+}
+
+SEC("tracepoint/syscalls/sys_enter_fsync")
+void tracepoint__syscalls__sys_enter_fsync(struct trace_event_raw_sys_enter *ctx)
+{
+	__syscall(ctx, SYS_FSYNC);
+}
+
+SEC("tracepoint/syscalls/sys_enter_fdatasync")
+void tracepoint__syscalls__sys_enter_fdatasync(struct trace_event_raw_sys_enter *ctx)
+{
+	__syscall(ctx, SYS_FDATASYNC);
+}
+
+SEC("tracepoint/syscalls/sys_enter_msync")
+void tracepoint__syscalls__sys_enter_msync(struct trace_event_raw_sys_enter *ctx)
+{
+	__syscall(ctx, SYS_MSYNC);
+}
+
+SEC("tracepoint/syscalls/sys_enter_sync_file_range")
+void tracepoint__syscalls__sys_enter_sync_file_range(struct trace_event_raw_sys_enter *ctx)
+{
+	__syscall(ctx, SYS_SYNC_FILE_RANGE);
+}
+
+SEC("tracepoint/syscalls/sys_enter_syncfs")
+void tracepoint__syscalls__sys_enter_syncfs(struct trace_event_raw_sys_enter *ctx)
+{
+	__syscall(ctx, SYS_SYNCFS);
 }
 
 char LICENSE[] SEC("license") = "GPL";
