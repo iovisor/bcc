16c774beb16f2439cb9479d73eaccb399d427e5c
Rong Tao <rongtao@cestc.cn>
2024-12-20 10:24:16 +0800

    libbpf-tools/mountsnoop: Support fsopen,fsconfig,fsmount,move_mount syscalls
    Do the same thing of bcc commit 60ebaf47347a ("tools/mountsnoop: Support
    fsopen(2), fsmount(2), fsconfig(2), move_mount(2)")
    
        $ sudo ./mountsnoop
        COMM             PID     TID     MNT_NS      CALL
        fsmount          431216  431216  4026531841  fsopen("ext4", 0x0) = 5
        fsmount          431216  431216  4026531841  fsconfig(5, "FSCONFIG_SET_FLAG", "rw", "", 0) = 0
        fsmount          431216  431216  4026531841  fsconfig(5, "FSCONFIG_SET_STRING", "source", "/dev/loop0", 0) = 0
        fsmount          431216  431216  4026531841  fsconfig(5, "FSCONFIG_CMD_CREATE", "", "", 0) = 0
        fsmount          431216  431216  4026531841  fsmount(5, "0x0", "MOUNT_ATTR_RDONLY") = 6
        fsmount          431216  431216  4026531841  move_mount(6, "", AT_FDCWD, "./tmp-dir/", "") = 0
        fsmount          431216  431216  4026531841  umount("./tmp-dir/", 0x0) = 0
    
    In the above test, the C program is more complicated, so I will not show it
    here, but a test example is given in the link [1].
    
    [1] https://github.com/torvalds/linux/blob/master/samples/vfs/test-fsmount.c
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- libbpf-tools/mountsnoop.h
+++ libbpf-tools/mountsnoop.h
@@ -11,27 +11,59 @@ enum op {
 	OP_MIN, /* skip 0 */
 	MOUNT,
 	UMOUNT,
+	FSOPEN,
+	FSCONFIG,
+	FSMOUNT,
+	MOVE_MOUNT,
+};
+
+union sys_arg {
+	/* op=MOUNT */
+	struct {
+		__u64 flags;
+		const char *src;
+		const char *dest;
+		const char *fs;
+		const char *data;
+	} mount;
+	/* op=UMOUNT */
+	struct {
+		__u64 flags;
+		const char *dest;
+	} umount;
+	/* op=FSOPEN */
+	struct {
+		const char *fs;
+		__u32 flags;
+	} fsopen;
+	/* op=FSCONFIG */
+	struct {
+		int fd;
+		unsigned int cmd;
+		const char *key;
+		const char *value;
+		int aux;
+	} fsconfig;
+	/* op=FSMOUNT */
+	struct {
+		int fs_fd;
+		__u32 flags;
+		__u32 attr_flags;
+	} fsmount;
+	/* op=MOVE_MOUNT */
+	struct {
+		int from_dfd;
+		const char *from_pathname;
+		int to_dfd;
+		const char *to_pathname;
+		__u32 flags;
+	} move_mount;
 };
 
 struct arg {
 	__u64 ts;
 	enum op op;
-
-	union {
-		/* op=MOUNT */
-		struct {
-			__u64 flags;
-			const char *src;
-			const char *dest;
-			const char *fs;
-			const char *data;
-		} mount;
-		/* op=UMOUNT */
-		struct {
-			__u64 flags;
-			const char *dest;
-		} umount;
-	};
+	union sys_arg sys;
 };
 
 struct event {
@@ -56,6 +88,33 @@ struct event {
 			__u64 flags;
 			char dest[PATH_MAX];
 		} umount;
+		/* op=FSOPEN */
+		struct {
+			char fs[FS_NAME_LEN];
+			__u32 flags;
+		} fsopen;
+		/* op=FSCONFIG */
+		struct {
+			int fd;
+			unsigned int cmd;
+			char key[DATA_LEN];
+			char value[DATA_LEN];
+			int aux;
+		} fsconfig;
+		/* op=FSMOUNT */
+		struct {
+			int fs_fd;
+			__u32 flags;
+			__u32 attr_flags;
+		} fsmount;
+		/* op=MOVE_MOUNT */
+		struct {
+			int from_dfd;
+			char from_pathname[PATH_MAX];
+			int to_dfd;
+			char to_pathname[PATH_MAX];
+			__u32 flags;
+		} move_mount;
 	};
 };
 
