e80ad4d859d4f6bf16709cb664c97f41e3136558
Rong Tao <rongtao@cestc.cn>
2025-02-01 08:02:08 +0800

    tools/opensnoop: Display mode for -e, --extended_fields (#5196)
    When a program creates a file with mode=0000, it cannot even access the file
    itself. It would be helpful if we could track the mode value. so we can know
    who did it.
    
    Example:
    
        open("a.txt", O_WRONLY | O_EXCL | O_CREAT, 0000);
    
    Then:
    
        $ ls -l a.txt
        ----------. 1 rongtao rongtao 0 Jan 24 09:07 a.txt
        $ cat a.txt
        cat: a.txt: Permission denied
    
        $ sudo ./opensnoop.py -e
        PID    COMM               FD ERR FLAGS    MODE PATH
        673067 open                3   0 00000301 n/a  a.txt
                                                  ^^^^
    
    If flags is an illegal value, displaying the mode value can better handle the
    relationship between flags and mode. After all, mode is only effective when
    flags only contains O_TMPFILE or O_CREAT.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tools/opensnoop.py
+++ tools/opensnoop.py
@@ -99,6 +99,7 @@ for flag in args.flag_filter or []:
 bpf_text = """
 #include <uapi/linux/ptrace.h>
 #include <uapi/linux/limits.h>
+#include <linux/fcntl.h>
 #include <linux/sched.h>
 #ifdef FULLPATH
 #include <linux/fs_struct.h>
@@ -117,6 +118,7 @@ struct val_t {
     char comm[TASK_COMM_LEN];
     const char *fname;
     int flags; // EXTENDED_STRUCT_MEMBER
+    u32 mode; // EXTENDED_STRUCT_MEMBER
 };
 
 struct data_t {
@@ -130,6 +132,7 @@ struct data_t {
 #endif
     char name[NAME_MAX];
     int flags; // EXTENDED_STRUCT_MEMBER
+    u32 mode; // EXTENDED_STRUCT_MEMBER
 };
 
 BPF_PERF_OUTPUT(events);
@@ -158,6 +161,7 @@ int trace_return(struct pt_regs *ctx)
     data.ts = tsp / 1000;
     data.uid = bpf_get_current_uid_gid();
     data.flags = valp->flags; // EXTENDED_STRUCT_MEMBER
+    data.mode = valp->mode; // EXTENDED_STRUCT_MEMBER
     data.ret = PT_REGS_RC(ctx);
 
     SUBMIT_DATA
@@ -169,12 +173,15 @@ int trace_return(struct pt_regs *ctx)
 """
 
 bpf_text_kprobe_header_open = """
-int syscall__trace_entry_open(struct pt_regs *ctx, const char __user *filename, int flags)
+int syscall__trace_entry_open(struct pt_regs *ctx, const char __user *filename,
+                              int flags, u32 mode)
 {
 """
 
 bpf_text_kprobe_header_openat = """
-int syscall__trace_entry_openat(struct pt_regs *ctx, int dfd, const char __user *filename, int flags)
+int syscall__trace_entry_openat(struct pt_regs *ctx, int dfd,
+                                const char __user *filename, int flags,
+                                u32 mode)
 {
 """
 
@@ -183,6 +190,10 @@ bpf_text_kprobe_header_openat2 = """
 int syscall__trace_entry_openat2(struct pt_regs *ctx, int dfd, const char __user *filename, struct open_how *how)
 {
     int flags = how->flags;
+    u32 mode = 0;
+
+    if (flags & (O_CREAT | O_TMPFILE))
+        mode = how->mode;
 """
 
 bpf_text_kprobe_body = """
@@ -204,6 +215,7 @@ bpf_text_kprobe_body = """
         val.id = id;
         val.fname = filename;
         val.flags = flags; // EXTENDED_STRUCT_MEMBER
+        val.mode = mode; // EXTENDED_STRUCT_MEMBER
         infotmp.update(&id, &val);
     }
 
@@ -217,8 +229,20 @@ KRETFUNC_PROBE(FNNAME, struct pt_regs *regs, int ret)
 {
     const char __user *filename = (char *)PT_REGS_PARM1(regs);
     int flags = PT_REGS_PARM2(regs);
+    u32 mode = 0;
+
+    /**
+     * open(2): The mode argument must be supplied if O_CREAT or O_TMPFILE is
+     * specified in flags; if it is not supplied, some arbitrary bytes from
+     * the stack will be applied as the file mode.
+     *
+     * Other O_CREAT | O_TMPFILE checks about flags are also for this reason.
+     */
+    if (flags & (O_CREAT | O_TMPFILE))
+        mode = PT_REGS_PARM3(regs);
 #else
-KRETFUNC_PROBE(FNNAME, const char __user *filename, int flags, int ret)
+KRETFUNC_PROBE(FNNAME, const char __user *filename, int flags,
+               u32 mode, int ret)
 {
 #endif
 """
@@ -230,8 +254,13 @@ KRETFUNC_PROBE(FNNAME, struct pt_regs *regs, int ret)
     int dfd = PT_REGS_PARM1(regs);
     const char __user *filename = (char *)PT_REGS_PARM2(regs);
     int flags = PT_REGS_PARM3(regs);
+    u32 mode = 0;
+
+    if (flags & (O_CREAT | O_TMPFILE))
+        mode = PT_REGS_PARM4(regs);
 #else
-KRETFUNC_PROBE(FNNAME, int dfd, const char __user *filename, int flags, int ret)
+KRETFUNC_PROBE(FNNAME, int dfd, const char __user *filename, int flags,
+               u32 mode, int ret)
 {
 #endif
 """
@@ -245,13 +274,21 @@ KRETFUNC_PROBE(FNNAME, struct pt_regs *regs, int ret)
     const char __user *filename = (char *)PT_REGS_PARM2(regs);
     struct open_how __user how;
     int flags;
+    u32 mode = 0;
 
     bpf_probe_read_user(&how, sizeof(struct open_how), (struct open_how*)PT_REGS_PARM3(regs));
     flags = how.flags;
+
+    if (flags & (O_CREAT | O_TMPFILE))
+        mode = how.mode;
 #else
 KRETFUNC_PROBE(FNNAME, int dfd, const char __user *filename, struct open_how __user *how, int ret)
 {
     int flags = how->flags;
+    u32 mode = 0;
+
+    if (flags & (O_CREAT | O_TMPFILE))
+        mode = how->mode;
 #endif
 """
 
@@ -278,6 +315,7 @@ bpf_text_kfunc_body = """
     data.ts    = tsp / 1000;
     data.uid   = bpf_get_current_uid_gid();
     data.flags = flags; // EXTENDED_STRUCT_MEMBER
+    data.mode  = mode; // EXTENDED_STRUCT_MEMBER
     data.ret   = ret;
 
     SUBMIT_DATA
@@ -406,7 +444,7 @@ if args.print_uid:
 print("%-6s %-16s %4s %3s " %
       ("TID" if args.tid else "PID", "COMM", "FD", "ERR"), end="")
 if args.extended_fields:
-    print("%-9s" % ("FLAGS"), end="")
+    print("%-8s %-4s " % ("FLAGS", "MODE"), end="")
 print("PATH")
 
 class EventType(object):
@@ -453,7 +491,12 @@ def print_event(cpu, data, size):
                     event.comm, fd_s, err), nl="")
 
             if args.extended_fields:
-                printb(b"%08o " % event.flags, nl="")
+                # If neither O_CREAT nor O_TMPFILE is specified in flags, then
+                # mode is ignored, see open(2).
+                if event.mode == 0 and event.flags & (os.O_CREAT | os.O_TMPFILE) == 0:
+                    printb(b"%08o n/a  " % event.flags, nl="")
+                else:
+                    printb(b"%08o %04o " % (event.flags, event.mode), nl="")
 
             if not args.full_path:
                 printb(b"%s" % event.name)
