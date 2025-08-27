iovisor/bcc

commit ca5fd8ee6fe03e8617e83de1d6d6a1e2994d668c

libbpf-tools: Add biotop tool.
biotop is a simple program that traces block I/O and print them like top.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

libbpf-tools/biotop.c
@@ -1,0 +1,451 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+
+/*
+ * biotop Trace block I/O by process.
+ * Copyright (c) 2022 Francis Laniel <flaniel@linux.microsoft.com>
+ *
+ * Based on biotop(8) from BCC by Brendan Gregg.
+ * 03-Mar-2022   Francis Laniel   Created this.
+ */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <argp.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include "biotop.h"
+#include "biotop.skel.h"
+#include "trace_helpers.h"
+
+#define warn(...) fprintf(stderr, __VA_ARGS__)
+#define OUTPUT_ROWS_LIMIT 10240
+
+enum SORT {
+	ALL,
+	IO,
+	BYTES,
+	TIME,
+};
+
+struct disk {
+	int major;
+	int minor;
+	char name[256];
+};
+
+struct vector {
+	size_t nr;
+	size_t capacity;
+	void **elems;
+};
+
+int grow_vector(struct vector *vector) {
+	if (vector->nr >= vector->capacity) {
+		void **reallocated;
+
+		if (!vector->capacity)
+			vector->capacity = 1;
+		else
+			vector->capacity *= 2;
+
+		reallocated = reallocarray(vector->elems, vector->capacity, sizeof(*vector->elems));
+		if (!reallocated)
+			return -1;
+
+		vector->elems = reallocated;
+	}
+
+	return 0;
+}
+
+void free_vector(struct vector vector) {
+	for (size_t i = 0; i < vector.nr; i++)
+		if (vector.elems[i] != NULL)
+			free(vector.elems[i]);
+	free(vector.elems);
+}
+
+struct vector disks = {};
+
+static volatile sig_atomic_t exiting = 0;
+
+static bool clear_screen = true;
+static int output_rows = 20;
+static int sort_by = ALL;
+static int interval = 1;
+static int count = 99999999;
+static bool verbose = false;
+
+const char *argp_program_version = "biotop 0.1";
+const char *argp_program_bug_address =
+	"https://github.com/iovisor/bcc/tree/master/libbpf-tools";
+const char argp_program_doc[] =
+"Trace file reads/writes by process.\n"
+"\n"
+"USAGE: biotop [-h] [interval] [count]\n"
+"\n"
+"EXAMPLES:\n"
+"    biotop            # file I/O top, refresh every 1s\n"
+"    biotop 5 10       # 5s summaries, 10 times\n";
+
+static const struct argp_option opts[] = {
+	{ "noclear", 'C', NULL, 0, "Don't clear the screen" },
+	{ "sort", 's', "SORT", 0, "Sort columns, default all [all, io, bytes, time]" },
+	{ "rows", 'r', "ROWS", 0, "Maximum rows to print, default 20" },
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	long rows;
+	static int pos_args;
+
+	switch (key) {
+	case 'C':
+		clear_screen = false;
+		break;
+	case 's':
+		if (!strcmp(arg, "all")) {
+			sort_by = ALL;
+		} else if (!strcmp(arg, "io")) {
+			sort_by = IO;
+		} else if (!strcmp(arg, "bytes")) {
+			sort_by = BYTES;
+		} else if (!strcmp(arg, "time")) {
+			sort_by = TIME;
+		} else {
+			warn("invalid sort method: %s\n", arg);
+			argp_usage(state);
+		}
+		break;
+	case 'r':
+		errno = 0;
+		rows = strtol(arg, NULL, 10);
+		if (errno || rows <= 0) {
+			warn("invalid rows: %s\n", arg);
+			argp_usage(state);
+		}
+		output_rows = rows;
+		if (output_rows > OUTPUT_ROWS_LIMIT)
+			output_rows = OUTPUT_ROWS_LIMIT;
+		break;
+	case 'v':
+		verbose = true;
+		break;
+	case 'h':
+		argp_state_help(state, stderr, ARGP_HELP_STD_HELP);
+		break;
+	case ARGP_KEY_ARG:
+		errno = 0;
+		if (pos_args == 0) {
+			interval = strtol(arg, NULL, 10);
+			if (errno || interval <= 0) {
+				warn("invalid interval\n");
+				argp_usage(state);
+			}
+		} else if (pos_args == 1) {
+			count = strtol(arg, NULL, 10);
+			if (errno || count <= 0) {
+				warn("invalid count\n");
+				argp_usage(state);
+			}
+		} else {
+			warn("unrecognized positional argument: %s\n", arg);
+			argp_usage(state);
+		}
+		pos_args++;
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	if (level == LIBBPF_DEBUG && !verbose)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void sig_int(int signo)
+{
+	exiting = 1;
+}
+
+struct data_t {
+	struct info_t key;
+	struct val_t value;
+};
+
+static int sort_column(const void *obj1, const void *obj2)
+{
+	struct data_t *d1 = (struct data_t *) obj1;
+	struct data_t *d2 = (struct data_t *) obj2;
+
+	struct val_t *s1 = &d1->value;
+	struct val_t *s2 = &d2->value;
+
+	if (sort_by == IO)
+		return s2->io - s1->io;
+	else if (sort_by == BYTES)
+		return s2->bytes - s1->bytes;
+	else if (sort_by == TIME)
+		return s2->us - s1->us;
+	else
+		return (s2->io + s2->bytes + s2->us)
+			- (s1->io + s1->bytes + s1->us);
+}
+
+static void parse_disk_stat(void)
+{
+	FILE *fp;
+	char *line;
+	size_t zero;
+
+	fp = fopen("/proc/diskstats", "r");
+	if (!fp)
+		return;
+
+	zero = 0;
+	while (getline(&line, &zero, fp) != -1) {
+		struct disk disk;
+
+		if (sscanf(line, "%d %d %s", &disk.major, &disk.minor, disk.name) != 3)
+			continue;
+
+		if (grow_vector(&disks) == -1)
+			goto err;
+
+		disks.elems[disks.nr] = malloc(sizeof(disk));
+		if (!disks.elems[disks.nr])
+			goto err;
+
+		memcpy(disks.elems[disks.nr], &disk, sizeof(disk));
+
+		disks.nr++;
+	}
+
+	free(line);
+	fclose(fp);
+
+	return;
+err:
+	fprintf(stderr, "realloc or malloc failed\n");
+
+	free_vector(disks);
+}
+
+static char *search_disk_name(int major, int minor)
+{
+	for (size_t i = 0; i < disks.nr; i++) {
+		struct disk *diskp;
+
+		if (!disks.elems[i])
+			continue;
+
+		diskp = (struct disk *) disks.elems[i];
+		if (diskp->major == major && diskp->minor == minor)
+			return diskp->name;
+	}
+
+	return "";
+}
+
+static int print_stat(struct biotop_bpf *obj)
+{
+	FILE *f;
+	time_t t;
+	struct tm *tm;
+	char ts[16], buf[256];
+	struct info_t *prev_key = NULL;
+	static struct data_t datas[OUTPUT_ROWS_LIMIT];
+	int n, i, err = 0, rows = 0;
+	int fd = bpf_map__fd(obj->maps.counts);
+
+	f = fopen("/proc/loadavg", "r");
+	if (f) {
+		time(&t);
+		tm = localtime(&t);
+		strftime(ts, sizeof(ts), "%H:%M:%S", tm);
+		memset(buf, 0, sizeof(buf));
+		n = fread(buf, 1, sizeof(buf), f);
+		if (n)
+			printf("%8s loadavg: %s\n", ts, buf);
+		fclose(f);
+	}
+	printf("%-7s %-16s %1s %-3s %-3s %-8s %5s %7s %6s\n",
+	       "PID", "COMM", "D", "MAJ", "MIN", "DISK", "I/O", "Kbytes", "AVGms");
+
+	while (1) {
+		err = bpf_map_get_next_key(fd, prev_key, &datas[rows].key);
+		if (err) {
+			if (errno == ENOENT) {
+				err = 0;
+				break;
+			}
+			warn("bpf_map_get_next_key failed: %s\n", strerror(errno));
+			return err;
+		}
+		err = bpf_map_lookup_elem(fd, &datas[rows].key, &datas[rows].value);
+		if (err) {
+			warn("bpf_map_lookup_elem failed: %s\n", strerror(errno));
+			return err;
+		}
+		prev_key = &datas[rows].key;
+		rows++;
+	}
+
+	qsort(datas, rows, sizeof(struct data_t), sort_column);
+	rows = rows < output_rows ? rows : output_rows;
+	for (i = 0; i < rows; i++) {
+		int major;
+		int minor;
+		struct info_t *key = &datas[i].key;
+		struct val_t *value = &datas[i].value;
+		float avg_ms = 0;
+
+		/* To avoid floating point exception. */
+		if (value->io)
+			avg_ms = ((float) value->us) / 1000 / value->io;
+
+		major = key->major;
+		minor = key->minor;
+
+		printf("%-7d %-16s %1s %-3d %-3d %-8s %5d %7lld %6.2f\n",
+		       key->pid, key->name, key->rwflag ? "W": "R",
+		       major, minor, search_disk_name(major, minor),
+		       value->io, value->bytes / 1024, avg_ms);
+	}
+
+	printf("\n");
+	prev_key = NULL;
+
+	while (1) {
+		struct info_t key;
+
+		err = bpf_map_get_next_key(fd, prev_key, &key);
+		if (err) {
+			if (errno == ENOENT) {
+				err = 0;
+				break;
+			}
+			warn("bpf_map_get_next_key failed: %s\n", strerror(errno));
+			return err;
+		}
+		err = bpf_map_delete_elem(fd, &key);
+		if (err) {
+			warn("bpf_map_delete_elem failed: %s\n", strerror(errno));
+			return err;
+		}
+		prev_key = &key;
+	}
+	return err;
+}
+
+int main(int argc, char **argv)
+{
+	static const struct argp argp = {
+		.options = opts,
+		.parser = parse_arg,
+		.doc = argp_program_doc,
+	};
+	struct biotop_bpf *obj;
+	struct ksyms *ksyms;
+	int err;
+
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err)
+		return err;
+
+	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+	libbpf_set_print(libbpf_print_fn);
+
+	obj = biotop_bpf__open();
+	if (!obj) {
+		warn("failed to open BPF object\n");
+		return 1;
+	}
+
+	parse_disk_stat();
+
+	ksyms = ksyms__load();
+	if (!ksyms) {
+		err = -ENOMEM;
+		warn("failed to load kallsyms\n");
+		goto cleanup;
+	}
+
+	err = biotop_bpf__load(obj);
+	if (err) {
+		warn("failed to load BPF object: %d\n", err);
+		goto cleanup;
+	}
+
+	if (ksyms__get_symbol(ksyms, "__blk_account_io_start"))
+		obj->links.blk_account_io_start = bpf_program__attach_kprobe(obj->progs.blk_account_io_start, false, "__blk_account_io_start");
+	else
+		obj->links.blk_account_io_start = bpf_program__attach_kprobe(obj->progs.blk_account_io_start, false, "blk_account_io_start");
+
+	if (!obj->links.blk_account_io_start) {
+		warn("failed to load attach blk_account_io_start\n");
+		goto cleanup;
+	}
+
+	if (ksyms__get_symbol(ksyms, "__blk_account_io_done"))
+		obj->links.blk_account_io_done = bpf_program__attach_kprobe(obj->progs.blk_account_io_done, false, "__blk_account_io_done");
+	else
+		obj->links.blk_account_io_done = bpf_program__attach_kprobe(obj->progs.blk_account_io_done, false, "blk_account_io_done");
+
+	if (!obj->links.blk_account_io_done) {
+		warn("failed to load attach blk_account_io_done\n");
+		goto cleanup;
+	}
+
+	err = biotop_bpf__attach(obj);
+	if (err) {
+		warn("failed to attach BPF programs: %d\n", err);
+		goto cleanup;
+	}
+
+	if (signal(SIGINT, sig_int) == SIG_ERR) {
+		warn("can't set signal handler: %s\n", strerror(errno));
+		err = 1;
+		goto cleanup;
+	}
+
+	while (1) {
+		sleep(interval);
+
+		if (clear_screen) {
+			err = system("clear");
+			if (err)
+				goto cleanup;
+		}
+
+		err = print_stat(obj);
+		if (err)
+			goto cleanup;
+
+		count--;
+		if (exiting || !count)
+			goto cleanup;
+	}
+
+cleanup:
+	ksyms__free(ksyms);
+	free_vector(disks);
+	biotop_bpf__destroy(obj);
+
+	return err != 0;
+}

libbpf-tools/.gitignore
@@ -6,6 +6,7 @@
 /biopattern
 /biosnoop
 /biostacks
+/biotop
 /bitesize
 /btrfsdist
 /btrfsslower

libbpf-tools/Makefile
@@ -41,6 +41,7 @@ APPS = \
 	biopattern \
 	biosnoop \
 	biostacks \
+	biotop \
 	bitesize \
 	cachestat \
 	cpudist \

libbpf-tools/biotop.bpf.c
@@ -1,0 +1,106 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2022 Francis Laniel <flaniel@linux.microsoft.com>
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+
+#include "biotop.h"
+#include "maps.bpf.h"
+#include "core_fixes.bpf.h"
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 10240);
+	__type(key, struct request *);
+	__type(value, struct start_req_t);
+} start SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 10240);
+	__type(key, struct request *);
+	__type(value, struct who_t);
+} whobyreq SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 10240);
+	__type(key, struct info_t);
+	__type(value, struct val_t);
+} counts SEC(".maps");
+
+SEC("kprobe")
+int BPF_KPROBE(blk_account_io_start, struct request *req)
+{
+	struct who_t who = {};
+
+	/* cache PID and comm by-req */
+	bpf_get_current_comm(&who.name, sizeof(who.name));
+	who.pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_map_update_elem(&whobyreq, &req, &who, 0);
+
+	return 0;
+}
+
+SEC("kprobe/blk_mq_start_request")
+int BPF_KPROBE(blk_mq_start_request, struct request *req)
+{
+	/* time block I/O */
+	struct start_req_t start_req;
+
+	start_req.ts = bpf_ktime_get_ns();
+	start_req.data_len = BPF_CORE_READ(req, __data_len);
+
+	bpf_map_update_elem(&start, &req, &start_req, 0);
+	return 0;
+}
+
+SEC("kprobe")
+int BPF_KPROBE(blk_account_io_done, struct request *req, u64 now)
+{
+	struct val_t *valp, zero = {};
+	struct info_t info = {};
+	struct start_req_t *startp;
+	unsigned int cmd_flags;
+	struct gendisk *disk;
+	struct who_t *whop;
+	u64 delta_us;
+
+	/* fetch timestamp and calculate delta */
+	startp = bpf_map_lookup_elem(&start, &req);
+	if (!startp)
+		return 0;    /* missed tracing issue */
+
+	delta_us = (bpf_ktime_get_ns() - startp->ts) / 1000;
+
+	/* setup info_t key */
+	cmd_flags = BPF_CORE_READ(req, cmd_flags);
+
+	disk = get_disk(req);
+	info.major = BPF_CORE_READ(disk, major);
+	info.minor = BPF_CORE_READ(disk, first_minor);
+	info.rwflag = !!((cmd_flags & REQ_OP_MASK) == REQ_OP_WRITE);
+
+	whop = bpf_map_lookup_elem(&whobyreq, &req);
+	if (whop) {
+		info.pid = whop->pid;
+		__builtin_memcpy(&info.name, whop->name, sizeof(info.name));
+	}
+
+	valp = bpf_map_lookup_or_try_init(&counts, &info, &zero);
+
+	if (valp) {
+		/* save stats */
+		valp->us += delta_us;
+		valp->bytes += startp->data_len;
+		valp->io++;
+	}
+
+	bpf_map_delete_elem(&start, &req);
+	bpf_map_delete_elem(&whobyreq, &req);
+
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";

libbpf-tools/biotop.h
@@ -1,0 +1,38 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#ifndef __BIOTOP_H
+#define __BIOTOP_H
+
+#define REQ_OP_BITS 8
+#define REQ_OP_MASK ((1 << REQ_OP_BITS) - 1)
+
+#define TASK_COMM_LEN 16
+
+/* for saving the timestamp and __data_len of each request */
+struct start_req_t {
+	__u64 ts;
+	__u64 data_len;
+};
+
+/* for saving process info by request */
+struct who_t {
+	__u32 pid;
+	char name[TASK_COMM_LEN];
+};
+
+/* the key for the output summary */
+struct info_t {
+	__u32 pid;
+	int rwflag;
+	int major;
+	int minor;
+	char name[TASK_COMM_LEN];
+};
+
+/* the value of the output summary */
+struct val_t {
+	__u64 bytes;
+	__u64 us;
+	__u32 io;
+};
+
+#endif /* __BIOTOP_H */

commit f7ea329a4ba66fb5cfbf78c2de934e87b931642a

libbpf-tools/biotop: Fix segmentation fault with musl libc build
When building with musl libc, biotop would crash with a segmentation
fault due to an uninitialized local variable line in the
parse_disk_stat() function.

This commit initializes the line variable to NULL to prevent the
crash.

Fixes: ca5fd8ee6fe0 ("libbpf-tools: Add biotop tool)
Signed-off-by: Ism Hong <ism.hong@gmail.com>

libbpf-tools/biotop.c
@@ -225,7 +225,7 @@ static int sort_column(const void *obj1, const void *obj2)
 static void parse_disk_stat(void)
 {
 	FILE *fp;
-	char *line;
+	char *line = NULL;
 	size_t zero;
 
 	fp = fopen("/proc/diskstats", "r");